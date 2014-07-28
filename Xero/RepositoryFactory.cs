0d29a55bd702dc2050246f570d1e65c04e34fb2d
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-07-28 15:51:06 +0100

    add in a class that should make us a repository

---
+++ Xero/RepositoryFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using DevDefined.OAuth.Logging;
+using DevDefined.OAuth.Storage.Basic;
+using XeroApi;
+using XeroApi.OAuth;
+
+namespace Xero
+{
+    internal class RepositoryFactory
+    {
+        private const string UserAgent = "Audition";
+        private const string ConsumerKey = "1PNBBUVEELJA2NIZ4DPALJ8UIAUS9H";
+        private const string ConsumerSecret = "OH9UCIP6NRRTR8BOPIIPI4YYXZNGYN";
+
+        public IRepository CreateRepository()
+        {
+            var consumerSession = new XeroApiPublicSession(UserAgent, ConsumerKey, ConsumerSecret,
+                new InMemoryTokenRepository())
+            {
+                MessageLogger = new DebugMessageLogger()
+            };
+
+            LaunchAuthenticationWindow(consumerSession);
+
+            var verificationCode = GetInputCode();
+
+            consumerSession.ExchangeRequestTokenForAccessToken(verificationCode);
+
+            // Wrap the authenticated consumerSession in the repository...
+            return new Repository(consumerSession);
+        }
+
+        private static string GetInputCode()
+        {
+            Console.WriteLine("Please input the code you were given in Xero:");
+            var verificationCode = Console.ReadLine();
+            return verificationCode;
+        }
+
+        private static void LaunchAuthenticationWindow(XeroApiPublicSession consumerSession)
+        {
+            var authorisationUrl = consumerSession.GetUserAuthorizationUrl();
+            Process.Start(authorisationUrl);
+        }
+    }
+}
