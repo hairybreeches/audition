8f4a679123a4e1cc270b331f8ad8f7775c155bf0
dconlin <dconlin@bitbucket.org>
2014-07-28 16:02:01 +0100

    couple of model classes to start to define what the xero module should do

---
+++ Model/IJournalSearcher.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Model
+{
+    public interface IJournalSearcher
+    {
+        IEnumerable<string> FindJournalsWithin(TimeFrame timeFrame);
+    }
+}
