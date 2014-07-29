d9486c91135cc392fc1d1e2a9726b3a737faac86
dconlin <dconlin@bitbucket.org>
2014-07-29 13:45:20 +0100

    first test, ensuring things pass through OK

---
+++ Tests/JournalExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+
+namespace Tests
+{
+    static class JournalExtensions
+    {
+        public static XeroApi.Model.Journal ToXeroJournal(this Journal xeroJournal)
+        {
+            return new XeroApi.Model.Journal
+            {
+                JournalID = xeroJournal.Id,
+                CreatedDateUTC = xeroJournal.Created,
+                JournalDate = xeroJournal.JournalDate
+            };
+        }
+    }
+}
