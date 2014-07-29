9b5d04885784cc27eece178495c2ed117f2763fc
dconlin <dconlin@bitbucket.org>
2014-07-29 09:19:36 +0100

    configure the app so I can use it to poke the Xero API

--- Audition/Program.cs
+++ Audition/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Autofac;
+using Model;
+using NodaTime;
+using Xero;
 
 namespace Audition
 {
@@ -10,6 +10,20 @@ namespace Audition
     {
         static void Main(string[] args)
         {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<XeroModule>();
+            using (var container = builder.Build())
+            {
+                var searcher = container.Resolve<IJournalSearcher>();
+                var journals =
+                    searcher.FindJournalsWithin(new TimeFrame(DayOfWeek.Monday, DayOfWeek.Friday, new LocalTime(9, 0),
+                        new LocalTime(5, 0)));
+
+                Console.WriteLine(String.Join(",\r\n", journals));
+                Console.WriteLine();
+            }
         }
+
+        
     }
 }
