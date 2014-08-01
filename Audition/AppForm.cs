36b846c9f18324b5f84cdf1bb5d6e0b53bdab2fe
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-08-01 17:07:50 +0100

    fit the viewport tothe search page

--- Audition/AppForm.cs
+++ Audition/AppForm.cs
@@ -12,7 +12,7 @@ namespace Audition
             
             Text = "Audition";
             Icon = Resources.AuditionIcon;
-            Size = new Size(820, 820);
+            Size = new Size(820, 565);
             FormBorderStyle = FormBorderStyle.FixedDialog;
             StartPosition = FormStartPosition.CenterParent;
         }
