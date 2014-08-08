de849267654f6c6fd193bca56f17f1f357e2e15a
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-08-08 10:53:38 +0100

    add in the concpet of a journal having lines

---
+++ Model/JournalLine.cs
@@ -0,0 +1,18 @@
+﻿namespace Model
+{
+    public class JournalLine
+    {
+        public JournalLine(string accountCode, string accountName, JournalType journalType, decimal amount)
+        {
+            AccountCode = accountCode;
+            AccountName = accountName;
+            JournalType = journalType;
+            Amount = amount;
+        }
+
+        public string AccountCode { get; private set; }
+        public string AccountName { get; private set; }        
+        public JournalType JournalType { get; private set; }
+        public decimal Amount { get; private set; }
+    }
+}
