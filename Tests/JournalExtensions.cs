fb44eb83640b118526259be9c2a399cc6ec3ee51
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-08-08 10:45:45 +0100

    rename variable

--- Tests/JournalExtensions.cs
+++ Tests/JournalExtensions.cs
@@ -4,13 +4,14 @@ namespace Tests
 {
     static class JournalExtensions
     {
-        public static XeroApi.Model.Journal ToXeroJournal(this Journal xeroJournal)
+        public static XeroApi.Model.Journal ToXeroJournal(this Journal modelJournal)
         {
             return new XeroApi.Model.Journal
             {
-                JournalID = xeroJournal.Id,
-                CreatedDateUTC = xeroJournal.Created,
-                JournalDate = xeroJournal.JournalDate
+                JournalID = modelJournal.Id,
+                CreatedDateUTC = modelJournal.Created,
+                JournalDate = modelJournal.JournalDate
+
             };
         }
     }
