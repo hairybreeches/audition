11cd1c30d1e40721e20921f5d24540b60f941062
hairybreeches <hairybreeches@bitbucket.org>
2014-08-11 11:37:55 +0100

    extract out a method for finding URLs for views

--- Audition/Chromium/WebConstants.cs
+++ Audition/Chromium/WebConstants.cs
@@ -1,7 +1,19 @@
-﻿namespace Audition.Chromium
+﻿using System;
+
+namespace Audition.Chromium
 {
     public static class WebConstants
     {
         public const string InternalDomain = @"http://localhost:1337";
+
+        public static string AddInternalDomain(string address)
+        {
+            return String.Format(@"{0}/{1}", InternalDomain, address);
+        }
+
+        public static string GetViewUrl(string location)
+        {
+            return AddInternalDomain("views/" + location);
+        }
     }
 }
