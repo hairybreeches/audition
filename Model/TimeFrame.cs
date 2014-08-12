b85c60789abd63c20caae6cb70e90dfb5187e6d2
hairybreeches <hairybreeches@bitbucket.org>
2014-08-12 17:48:59 +0100

    make the JSONConverter able to convert a TimeFrame

--- Model/TimeFrame.cs
+++ Model/TimeFrame.cs
@@ -1,4 +1,6 @@
 using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using NodaTime;
 
 namespace Model
@@ -17,7 +19,9 @@ namespace Model
             FromDay = fromDay;
         }
 
+        [JsonConverter(typeof(StringEnumConverter))]
         public DayOfWeek FromDay { get; private set; }
+        [JsonConverter(typeof(StringEnumConverter))]
         public DayOfWeek ToDay { get; private set; }
         public LocalTime FromTime { get; private set; }
         public LocalTime ToTime { get; private set; }
@@ -25,6 +29,31 @@ namespace Model
         public override string ToString()
         {
             return String.Format("From: {0} to {1}, {2} to {3}", FromDay, ToDay, FromTime, ToTime);
+        }
+
+        protected bool Equals(TimeFrame other)
+        {
+            return FromDay == other.FromDay && ToDay == other.ToDay && FromTime.Equals(other.FromTime) && ToTime.Equals(other.ToTime);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((TimeFrame) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (int) FromDay;
+                hashCode = (hashCode*397) ^ (int) ToDay;
+                hashCode = (hashCode*397) ^ FromTime.GetHashCode();
+                hashCode = (hashCode*397) ^ ToTime.GetHashCode();
+                return hashCode;
+            }
         }
     }
 }
