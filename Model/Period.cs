a927ba3f37c84a16eed0461e46e13a9e81314a63
hairybreeches <hairybreeches@bitbucket.org>
2014-08-12 10:58:56 +0100

    new classes for my searching

---
+++ Model/Period.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Model
+{
+    public class Period
+    {
+        public Period(DateTime fromDate, DateTime to)
+        {
+            To = to;
+            From = fromDate;
+        }
+
+        public DateTime From { get; private set; }
+        public DateTime To { get; private set; }
+    }
+}
