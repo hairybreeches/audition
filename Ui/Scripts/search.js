ac93f107131df456416c64215058da26a385779a
hairybreeches <hairybreeches@bitbucket.org>
2014-08-12 16:08:42 +0100

    get data from the search controller when the user presses the button

--- Ui/Scripts/search.js
+++ Ui/Scripts/search.js
@@ -1,14 +1,24 @@
 ﻿var model = ko.mapping.fromJS({
-    Period: {
-        From: "5/4/2013",
-        To: "4/4/2013"
-    },
+        input: {
+            Period: {
+                From: "5/4/2013",
+                To: "4/4/2013"
+            },
 
-    TimeFrame: {
-        FromDay: "Monday",
-        ToDay: "Friday",
-        FromTime: "08:00",
-        ToTime: "18:00"
-    }
-});
+            TimeFrame: {
+                FromDay: "Monday",
+                ToDay: "Friday",
+                FromTime: "08:00",
+                ToTime: "18:00"
+            }
+        },
+        submit: function(data, e) {            
+            e.preventDefault();
+            $.get('/api/search', data.input, function(output) {
+                console.log(output);
+            });
+        },
+
+        output:[]
+    });
 ko.applyBindings(model);
