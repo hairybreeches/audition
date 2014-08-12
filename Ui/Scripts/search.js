004c23d4d46ed2a02fb7a185946bb1bbb4d830e8
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-08-12 15:41:57 +0100

    ...get them from ko binding instead

---
+++ Ui/Scripts/search.js
@@ -0,0 +1,14 @@
+﻿var model = ko.mapping.fromJS({
+    Period: {
+        From: "5/4/2013",
+        To: "4/4/2013"
+    },
+
+    TimeFrame: {
+        FromDay: "Monday",
+        ToDay: "Friday",
+        FromTime: "08:00",
+        ToTime: "18:00"
+    }
+});
+ko.applyBindings(model);
