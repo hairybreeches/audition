6a3e3643102034977ffc0d1271dafdb945f84d3a
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-08-12 18:03:47 +0100

    make the converter able to deserialize a whole SearchWindow

--- Model/SearchWindow.cs
+++ Model/SearchWindow.cs
@@ -3,12 +3,39 @@ namespace Model
 {
     public class SearchWindow
     {
-        TimeFrame TimeFrame { get; set; }
-        Period Period { get; set; }
+        public SearchWindow(TimeFrame timeFrame, Period period)
+        {
+            Period = period;
+            TimeFrame = timeFrame;
+        }
+
+        public TimeFrame TimeFrame { get; private set; }
+        public Period Period { get; private set; }
 
         public override string ToString()
         {
             return string.Format("{0}, in the period {1}", TimeFrame, Period);
+        }
+
+        protected bool Equals(SearchWindow other)
+        {
+            return Equals(TimeFrame, other.TimeFrame) && Equals(Period, other.Period);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((SearchWindow) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((TimeFrame != null ? TimeFrame.GetHashCode() : 0)*397) ^ (Period != null ? Period.GetHashCode() : 0);
+            }
         }
     }
 }
