66782b79544cd9b44e58b45375bd4293319d9cd7
hairybreeches <hairybreeches@bitbucket.org>
2014-08-13 11:39:35 +0100

    new class and a method for converting it

---
+++ Audition/Chromium/CefSharpResponse.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System.IO;
+
+namespace Audition.Chromium
+{
+    internal class CefSharpResponse
+    {
+        public Stream Content { get; private set; }
+        public string Mime { get; private set; }
+        public string ReasonPhrase { get; private set; }
+        public int StatusCode { get; private set; }
+        public Dictionary<string, string> Headers { get; private set; }
+
+        public CefSharpResponse(Stream content, string mime, string reasonPhrase, int statusCode, Dictionary<string, string> headers)
+        {
+            Content = content;
+            Mime = mime;
+            ReasonPhrase = reasonPhrase;
+            StatusCode = statusCode;
+            Headers = headers;
+        }
+    }
+}
