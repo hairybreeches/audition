276a765923cd5e470d535df083fe04c4c10509cd
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-08-18 14:37:11 +0100

    extract to separate class

---
+++ Tests/TimeFrameTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Model;
+using NodaTime;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TimeFrameTests
+    {
+        [Test]
+        public void CannotCreateATimeFrameWithTimesWhichWrapAround()
+        {
+            Assert.Throws<InvalidTimeFrameException>(
+                () => new TimeFrame(DayOfWeek.Monday, DayOfWeek.Saturday, new LocalTime(16, 0), new LocalTime(15, 0)));
+        }
+    }
+}
