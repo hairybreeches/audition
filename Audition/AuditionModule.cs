90515a48ac8a4672bf4f4271403799dfa2325f60
hairybreeches <hairybreeches@bitbucket.org>
2014-08-18 11:16:51 +0100

    put all the di stuff in one module

---
+++ Audition/AuditionModule.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Audition.Chromium;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Xero;
+using Module = Autofac.Module;
+
+namespace Audition
+{
+    internal class AuditionModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterModule<XeroModule>();
+            builder.RegisterModule<ChromiumModule>();
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());     
+        }
+    }
+}
