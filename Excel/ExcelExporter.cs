6c4ce977f2e360ff0f74b6546cda78978a809507
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-08-18 10:38:30 +0100

    start to being able to export lists of journals to excel

---
+++ Excel/ExcelExporter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Model;
+
+namespace Excel
+{
+    public class ExcelExporter
+    {
+        private readonly IFileSystem fileSystem;
+
+        public ExcelExporter(IFileSystem fileSystem)
+        {
+            this.fileSystem = fileSystem;
+        }
+
+        public void WriteJournals(IEnumerable<Journal> journals, string filename)
+        {
+            using (var writer = CreateWriter(filename))
+            {
+                writer.WriteRecords(journals);
+                
+            }
+        }
+
+        private CsvWriter CreateWriter(string filename)
+        {
+            var csvWriter = new CsvWriter(fileSystem.OpenFileToWrite(filename));
+            csvWriter.Configuration.RegisterClassMap<JournalMap>();
+            return csvWriter;
+        }        
+    }
+
+    public class JournalMap : CsvClassMap<Journal>
+    {
+        public JournalMap()
+        {
+            Map(m => m.Created).Name("Created");
+            Map(m => m.JournalDate).Name("Date");
+        }
+    }
+}
---
+++ Tests/ExcelExporterTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Excel;
+using Model;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ExcelExporterTests
+    {
+        [Test]
+        public void ExportsJournalsCorrectly()
+        {
+            var fileSystem = new MockFileSystem();
+            var exporter = new ExcelExporter(fileSystem);
+            exporter.WriteJournals(new List<Journal>
+            {
+                new Journal(Guid.NewGuid(), new DateTime(2012,3,4),new DateTime(2012,3,4), new List<JournalLine>
+                {
+                    new JournalLine("9012", "Expenses", JournalType.Cr, 23.4m),
+                    new JournalLine("3001", "Cash", JournalType.Dr, 23.4m)
+                }  ),
+                new Journal(Guid.NewGuid(), new DateTime(2012,6,5),new DateTime(2012,6,5), new List<JournalLine>
+                {
+                    new JournalLine("9012", "Expenses", JournalType.Cr, 12.4m),
+                    new JournalLine("3001", "Cash", JournalType.Dr, 12.4m)
+                }  )
+            }, "c:\\steve.csv"
+            );
+
+            var expected = 
+@"Created,Date
+04/03/2012 00:00:00,04/03/2012 00:00:00
+05/06/2012 00:00:00,05/06/2012 00:00:00
+";
+            Assert.AreEqual(expected, fileSystem.GetFileValue("c:\\steve.csv"));
+        }
+    }
+}
