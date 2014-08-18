9c1e4a8575a610ae9604f3f609ee4ccd37bc7e41
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-08-18 16:46:22 +0100

    return the correct data for Xero

--- Audition/Controllers/SearchController.cs
+++ Audition/Controllers/SearchController.cs
@@ -20,19 +20,7 @@ namespace Audition.Controllers
         [Route("api/search")]
         public IEnumerable<Journal> Search([ModelBinder(typeof(JsonModelBinder))] SearchWindow searchWindow)
         {
-            return new List<Journal>
-            {
-                new Journal(Guid.NewGuid(), new DateTime(2012,3,4),new DateTime(2012,3,4), new List<JournalLine>
-                {
-                    new JournalLine("9012", "Expenses", JournalType.Cr, 23.4m),
-                    new JournalLine("3001", "Cash", JournalType.Dr, 23.4m)
-                }  ),
-                new Journal(Guid.NewGuid(), new DateTime(2012,6,5),new DateTime(2012,6,5), new List<JournalLine>
-                {
-                    new JournalLine("9012", "Expenses", JournalType.Cr, 12.4m),
-                    new JournalLine("3001", "Cash", JournalType.Dr, 12.4m)
-                }  )
-            };            
+            return searcher.FindJournalsWithin(searchWindow);
         }
     }
 }
