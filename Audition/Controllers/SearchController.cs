fea9a32fec88c06cf8a0ebecb3be725e26d0ceb8
hairybreeches <hairybreeches@bitbucket.org>
2014-08-19 13:16:18 +0100

    implement the Export method on the SearchController

--- Audition/Controllers/SearchController.cs
+++ Audition/Controllers/SearchController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using System.Web.Http.ModelBinding;
+using Excel;
 using Model;
 
 namespace Audition.Controllers
@@ -10,10 +11,12 @@ namespace Audition.Controllers
     public class SearchController : ApiController
     {
         private readonly IJournalSearcher searcher;
+        private readonly ExcelExporter excelExporter;
 
-        public SearchController(IJournalSearcher searcher)
+        public SearchController(IJournalSearcher searcher, ExcelExporter excelExporter)
         {
             this.searcher = searcher;
+            this.excelExporter = excelExporter;
         }
 
         [HttpGet]
@@ -21,6 +24,14 @@ namespace Audition.Controllers
         public IEnumerable<Journal> Search([ModelBinder(typeof(JsonModelBinder))] SearchWindow searchWindow)
         {
             return searcher.FindJournalsWithin(searchWindow);
+        }
+        
+        [HttpPost]
+        [Route("api/search/export")]
+        public void Export([ModelBinder(typeof(JsonModelBinder))] SaveSearchRequest saveRequest)
+        {
+            var journals = searcher.FindJournalsWithin(saveRequest.SearchWindow);
+            excelExporter.WriteJournals(journals, saveRequest.Filename);
         }
     }
 }
---
+++ Audition/SaveSearchRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Audition
+{
+    public class SaveSearchRequest
+    {
+        public SearchWindow SearchWindow { get; private set; }
+        public string Filename { get; private set; }
+
+        public SaveSearchRequest(SearchWindow searchWindow, string filename)
+        {
+            SearchWindow = searchWindow;
+            Filename = filename;
+        }
+    }
+}
