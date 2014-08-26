947282db1c4951cb478571d4e32592a75840d9b5
hairybreeches <hairybreeches@bitbucket.org>
2014-08-26 12:17:01 +0100

    failing test to show file export not working

--- SystemTests/SystemTests.cs
+++ SystemTests/SystemTests.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Audition;
+using Autofac;
+using CefSharp;
+using Model;
 using NUnit.Framework;
+using Tests.Mocks;
+using Xero;
 
 namespace SystemTests
 {
@@ -13,7 +20,52 @@ namespace SystemTests
         [Test]
         public void CanSaveJournalsReturnedToAFile()
         {
-            
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<AuditionModule>();
+            builder.Register(
+                _ =>
+                    MockXeroRepositoryFactory.Create(
+                        new Journal(Guid.NewGuid(), new DateTime(2013, 4, 6), new DateTime(2013, 4, 6),
+                            new List<JournalLine>
+                            {
+                                new JournalLine("9012", "Expenses", JournalType.Cr, 23.4m),
+                                new JournalLine("3001", "Cash", JournalType.Dr, 23.4m)
+                            }),
+                        new Journal(Guid.NewGuid(), new DateTime(2013, 4, 6), new DateTime(2013, 4, 6),
+                            new List<JournalLine>
+                            {
+                                new JournalLine("8014", "Depreciation", JournalType.Cr, 12.4m),
+                                new JournalLine("4001", "Fixed assets", JournalType.Dr, 12.4m)
+                            }),
+                    //this one is outside the financial period
+                        new Journal(Guid.NewGuid(), new DateTime(2012, 6, 5), new DateTime(2012, 6, 5),
+                            new List<JournalLine>
+                            {
+                                new JournalLine("8014", "Depreciation", JournalType.Cr, 12.4m),
+                                new JournalLine("4001", "Fixed assets", JournalType.Dr, 12.4m)
+                            }),
+                    //this one is "boring"
+                            new Journal(Guid.NewGuid(), new DateTime(2014, 4, 2, 12, 0, 0), new DateTime(2013, 6, 5),
+                            new List<JournalLine>
+                            {
+                                new JournalLine("8014", "Depreciation", JournalType.Cr, 12.4m),
+                                new JournalLine("4001", "Fixed assets", JournalType.Dr, 12.4m)
+                            }))).As<IRepositoryFactory>();
+
+            builder.Register(_ => new Microsoft.Owin.FileSystems.PhysicalFileSystem("."))
+                .As<Microsoft.Owin.FileSystems.IFileSystem>();
+
+            using (var lifetime = builder.Build())
+            {
+                var fileName = Path.GetTempFileName();
+                var handler = lifetime.Resolve<IRequestHandler>();
+
+                handler.OnBeforeResourceLoad(null, new MockRequestResponse("POST", "{'searchWindow':{'Period':{'From':'2013-4-5','To':'2014-4-4'},'Outside':{'FromDay':'Monday','ToDay':'Friday','FromTime':'08:00:00','ToTime':'18:00:00'}},'fileName':'" + fileName + "'}", "application/json", "http://localhost:1337/api/search/export"));
+                var fileContents = File.ReadAllText(fileName);
+                StringAssert.AreEqualIgnoringCase("steve", fileContents);
+            }
+
+
         }
     }
 }
