6a9e7069ef8f7a3e83a831ab71cd297c9c45c7b5
hairybreeches <hairybreeches@bitbucket.org>
2014-08-26 11:36:40 +0100

    new project for writing some system tests

---
+++ SystemTests/SystemTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SystemTests
+{
+    [TestFixture]
+    public class SystemTests
+    {
+        [Test]
+        public void CanSaveJournalsReturnedToAFile()
+        {
+            
+        }
+    }
+}
