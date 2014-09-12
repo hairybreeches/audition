81fdcd8f8e2869304403e4e03d4a71c0459e3426
hairybreeches <hairybreeches@bitbucket.org>
2014-09-12 09:43:46 +0100

    Add a logout method

--- Xero/RepositoryFactory.cs
+++ Xero/RepositoryFactory.cs
@@ -9,7 +9,7 @@ namespace Xero
 {
     public class RepositoryFactory : IRepositoryFactory
     {
-        private readonly XeroApiPublicSession xeroApiPublicSession;
+        private XeroApiPublicSession xeroApiPublicSession;
         private const string UserAgent = "Audition";
         private const string ConsumerKey = "1PNBBUVEELJA2NIZ4DPALJ8UIAUS9H";
         private const string ConsumerSecret = "OH9UCIP6NRRTR8BOPIIPI4YYXZNGYN";
@@ -17,12 +17,22 @@ namespace Xero
 
         public RepositoryFactory()
         {
+            CreateNewSession();            
+        }
+
+        private void CreateNewSession()
+        {
             xeroApiPublicSession = new XeroApiPublicSession(UserAgent, ConsumerKey, ConsumerSecret,
                 new InMemoryTokenRepository())
             {
                 MessageLogger = new DebugMessageLogger()
             };
-        }        
+        }
+
+        public void Logout()
+        {
+            CreateNewSession();
+        }
 
         public IFullRepository CreateRepository()
         {           
