50ad52cebb7d38c6e2039ce31e293a7603d2ce33
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-09-12 09:58:29 +0100

    put in a working cancel button from the finish xero login screen

--- Audition/Controllers/XeroSessionController.cs
+++ Audition/Controllers/XeroSessionController.cs
@@ -29,7 +29,7 @@ namespace Audition.Controllers
             return RedirectToView("search.html");
         }
 
-        [HttpPost]
+        [HttpGet]
         [Route(Routing.XeroLogout)]
         public IHttpActionResult Logout()
         {
