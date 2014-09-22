2a7445b1bb16432a865ffe475d4917d2d2244ded
hairybreeches <hairybreeches@bitbucket.org>
2014-09-22 14:07:17 +0100

    model representing the parameters for an "hours" search

---
+++ Model/AccountsSearchWindow.cs
@@ -0,0 +1,19 @@
+namespace Model
+{
+    public class AccountsSearchWindow
+    {
+        public AccountsSearchWindow(int quantity, DateRange period)
+        {
+            Quantity = quantity;
+            Period = period;
+        }
+        
+        public int Quantity { get; private set; }
+        public DateRange Period { get; private set; }
+
+        public override string ToString()
+        {
+            return string.Format("Fewer than {0} entries, in the period {1}", Quantity, Period);
+        }        
+    }
+}
