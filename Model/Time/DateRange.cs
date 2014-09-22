ae6af8ed2a51702bc242c296396645957712a4ea
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-09-22 14:09:52 +0100

    create namespace "model.time"

--- Model/Time/DateRange.cs
+++ Model/Time/DateRange.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Model
+namespace Model.Time
 {
     public class DateRange
     {
--- Model/Time/InvalidTimeFrameException.cs
+++ Model/Time/InvalidTimeFrameException.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Runtime.Serialization;
 
-namespace Model
+namespace Model.Time
 {
     public class InvalidTimeFrameException : Exception
     {
