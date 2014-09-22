6410521dedfb877e1b44e92f33665a18da6f38b6
hairybreeches <hairybreeches@bitbucket.org>
2014-09-22 16:59:04 +0100

    implement equality

--- Model/SearchWindows/AccountsSearchWindow.cs
+++ Model/SearchWindows/AccountsSearchWindow.cs
@@ -15,6 +15,24 @@ namespace Model.SearchWindows
         public override string ToString()
         {
             return string.Format("Fewer than {0} entries", Quantity);
-        }        
+        }
+
+        protected bool Equals(UnusualAccountsParameters other)
+        {
+            return Quantity == other.Quantity;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((UnusualAccountsParameters) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Quantity;
+        }
     }
 }
