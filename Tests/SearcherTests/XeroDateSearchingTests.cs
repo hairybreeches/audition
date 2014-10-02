0bb826a42bded623c5337e0682fcfdc93f956105
hairybreeches <hairybreeches@bitbucket.org>
2014-10-02 12:09:46 +0100

    new test

---
+++ Tests/SearcherTests/XeroDateSearchingTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Accounting;
+using Model.SearchWindows;
+using Model.Time;
+using NUnit.Framework;
+using Tests.Mocks;
+
+namespace Tests.SearcherTests
+{
+    [TestFixture]
+    public class XeroDateSearchingTests
+    {
+        private static readonly DateTime YearEnd = new DateTime(2012, 3, 31);
+        private static readonly DateRange FinancialPeriod = new DateRange(YearEnd.Subtract(TimeSpan.FromDays(365)), YearEnd);
+
+        [Test]
+        public void ReturnsJournalsPostedAfterYearEnd()
+        {
+            
+            var postYearEndJournal = new Journal(Guid.NewGuid(), YearEnd.AddDays(1), YearEnd.Subtract(TimeSpan.FromDays(60)),Enumerable.Empty<JournalLine>() );
+            var searcher = Mock.JournalSearcher(postYearEndJournal);
+
+            var searchParameters = new SearchWindow<YearEndParameters>(new YearEndParameters(2),
+                FinancialPeriod);
+
+            var result = searcher.FindJournalsWithin(searchParameters);
+            CollectionAssert.AreEquivalent(new []{postYearEndJournal}, result);
+        }
+    }
+}
