eb4fdcd352dd97a56e88f72b6f4fadcca85ef658
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-10-02 11:35:04 +0100

    move journal construction methods into Mock

--- Tests/SearcherTests/XeroAccountsSearchingTests.cs
+++ Tests/SearcherTests/XeroAccountsSearchingTests.cs
@@ -15,9 +15,9 @@ namespace Tests.SearcherTests
         public void ReturnsOnlyJournalsPostedToLessUsedAccounts()
         {
             //given one journal which includes a line to a rare account (one posting)
-            var journalPostedToUncommonAccount = JournalPostedTo("b", "e");
+            var journalPostedToUncommonAccount = Mock.JournalPostedTo("b", "e");
             //and several to accounts with >= 2 postings
-            var searcher = Mock.JournalSearcher(JournalPostedTo("a", "b"), JournalPostedTo("b", "a"), journalPostedToUncommonAccount);
+            var searcher = Mock.JournalSearcher(Mock.JournalPostedTo("a", "b"), Mock.JournalPostedTo("b", "a"), journalPostedToUncommonAccount);
 
             //and a search window for the period "all time", for journals to accounts with <2 postings
             var searchWindow = new SearchWindow<UnusualAccountsParameters>(new UnusualAccountsParameters(2),
@@ -34,10 +34,10 @@ namespace Tests.SearcherTests
         public void DoesNotReturnDuplicatesWhenJournalsPostedToTwoUnusualAccounts()
         {
             //given one journal which includes a line to two rare accounts (just one posting each)
-            var journalPostedToUncommonAccount = JournalPostedTo("d", "e");
+            var journalPostedToUncommonAccount = Mock.JournalPostedTo("d", "e");
 
             //and several to accounts with >= 2 postings
-            var searcher = Mock.JournalSearcher(JournalPostedTo("a", "b"), JournalPostedTo("b", "a"), journalPostedToUncommonAccount);
+            var searcher = Mock.JournalSearcher(Mock.JournalPostedTo("a", "b"), Mock.JournalPostedTo("b", "a"), journalPostedToUncommonAccount);
 
             //and a search window for the period "all time", for journals to accounts with <2 postings
             var searchWindow = new SearchWindow<UnusualAccountsParameters>(new UnusualAccountsParameters(2),
@@ -54,7 +54,7 @@ namespace Tests.SearcherTests
         public void DoesNotReturnJournalsOutsideThePeriod()
         {
             //given one journal which includes a line to two rare accounts, but does not apply to the period
-            var journal = JournalPostedTo("d", "e", new DateTime(2000, 4, 5));                
+            var journal = Mock.JournalPostedTo("d", "e", new DateTime(2000, 4, 5));                
 
             //and a search window for journals to accounts with <2 postings
             var searchWindow = new SearchWindow<UnusualAccountsParameters>(new UnusualAccountsParameters(2),
@@ -73,12 +73,12 @@ namespace Tests.SearcherTests
         public void JournalsOutsideThePeriodNotUsedToDetermineWhetherAccountCodeIsUnusual()
         {
             //given one journal inside the period to an account
-            var journal = JournalPostedTo("a", "b", new DateTime(1999, 1, 1));
+            var journal = Mock.JournalPostedTo("a", "b", new DateTime(1999, 1, 1));
             //and a load posted to the same account, but outside the period
             var searcher = Mock.JournalSearcher(journal, 
-                JournalPostedTo("a", "b", new DateTime(2000,1,1)),
-                JournalPostedTo("a", "b", new DateTime(1998,12,31)),
-                JournalPostedTo("a", "b", new DateTime(2000,1,1)));
+                Mock.JournalPostedTo("a", "b", new DateTime(2000,1,1)),
+                Mock.JournalPostedTo("a", "b", new DateTime(1998,12,31)),
+                Mock.JournalPostedTo("a", "b", new DateTime(2000,1,1)));
 
 
             //and a search window for journals to accounts with <2 postings
@@ -90,21 +90,6 @@ namespace Tests.SearcherTests
 
             //those journals posted to the account in other periods don't make the account any less unusual.
             CollectionAssert.AreEquivalent(new[]{journal}, journals);
-        }
-
-        private static Journal JournalPostedTo(string accountCode1, string accountCode2)
-        {
-            return JournalPostedTo(accountCode1, accountCode2, new DateTime(1999, 12, 1));
-        }
-
-        private static Journal JournalPostedTo(string accountCode1, string accountCode2, DateTime journalDate)
-        {
-            return new Journal(Guid.NewGuid(), new DateTime(1999, 12, 1), journalDate,
-                new[]
-                {
-                    new JournalLine(accountCode1, accountCode1, JournalType.Cr, 2.2m),
-                    new JournalLine(accountCode2, accountCode2, JournalType.Dr, 2.2m)
-                });
         }
     }
 }
