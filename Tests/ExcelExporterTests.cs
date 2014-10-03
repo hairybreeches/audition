40fdcf4cb836b9148e5b8da1d05c4d8d6ccec7a3
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-10-03 16:37:45 +0100

    update expectations for writing to file

--- Tests/ExcelExporterTests.cs
+++ Tests/ExcelExporterTests.cs
@@ -20,12 +20,15 @@ namespace Tests
             var exporter = new ExcelExporter(fileSystem);
             exporter.WriteJournals(new List<Journal>
             {
-                new Journal(Guid.NewGuid(), new DateTime(2012,3,4),new DateTime(2012,3,4), new List<JournalLine>
+                //one inside daylight savings
+                new Journal(Guid.NewGuid(), new DateTimeOffset(new DateTime(2012,3,4), TimeSpan.Zero),new DateTime(2012,3,4), new List<JournalLine>
                 {
                     new JournalLine("9012", "Expenses", JournalType.Cr, 23.4m),
                     new JournalLine("3001", "Cash", JournalType.Dr, 23.4m)
                 }  ),
-                new Journal(Guid.NewGuid(), new DateTime(2012,6,5),new DateTime(2012,6,5), new List<JournalLine>
+
+                //and one outside
+                new Journal(Guid.NewGuid(), new DateTimeOffset(new DateTime(2012,6,5), TimeSpan.FromHours(1)),new DateTime(2012,6,5), new List<JournalLine>
                 {
                     new JournalLine("8014", "Depreciation", JournalType.Cr, 12.4m),
                     new JournalLine("4001", "Fixed assets", JournalType.Dr, 12.4m)
@@ -35,8 +38,8 @@ namespace Tests
 
             var expected =
 @"Created,Date
-04/03/2012 00:00:00,04/03/2012 00:00:00,Cr,9012,Expenses,23.4,Dr,3001,Cash,23.4
-05/06/2012 00:00:00,05/06/2012 00:00:00,Cr,8014,Depreciation,12.4,Dr,4001,Fixed assets,12.4
+04/03/2012 00:00:00 +00:00,04/03/2012 00:00:00,Cr,9012,Expenses,23.4,Dr,3001,Cash,23.4
+05/06/2012 00:00:00 +01:00,05/06/2012 00:00:00,Cr,8014,Depreciation,12.4,Dr,4001,Fixed assets,12.4
 ";
             Assert.AreEqual(expected, fileSystem.GetFileValue("c:\\steve.csv"));
         }
