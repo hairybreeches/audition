08954e02cfd7f4d4aa965fd78d61402aefb89f8d
hairybreeches <hairybreeches@bitbucket.org>
2014-10-09 12:43:07 +0100

    login using username and password (currently ignoring ID)

--- Audition/Controllers/Sage50SessionController.cs
+++ Audition/Controllers/Sage50SessionController.cs
@@ -8,16 +8,19 @@ namespace Audition.Controllers
     public class Sage50SessionController : RedirectController
     {
         private readonly LoginSession session;
+        private readonly SearcherFactory factory;
 
-        public Sage50SessionController(LoginSession session)
+        public Sage50SessionController(LoginSession session, SearcherFactory factory)
         {
             this.session = session;
+            this.factory = factory;
         }
 
         [HttpPost]
         [Route(Routing.Sage50Login)]
         public IHttpActionResult Login(Sage50LoginDetails loginDetails)
         {            
+            session.Login(factory.CreateJournalSearcher(loginDetails));
             return RedirectToView("search.html");
         }               
 
