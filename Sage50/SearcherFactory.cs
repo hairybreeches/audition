a81bf52fd4cc311d8ffb243102d1176c8104d18c
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-10-09 14:18:30 +0100

    reallocate responsibilities

--- Sage50/SearcherFactory.cs
+++ Sage50/SearcherFactory.cs
@@ -9,16 +9,42 @@ namespace Sage50
     {
         public IJournalSearcher CreateJournalSearcher(Sage50LoginDetails loginDetails)
         {
-            TestLoginDetails(loginDetails);
-            return new JournalSearcher(loginDetails);
+            return new JournalSearcher(CreateConnectionFactory(loginDetails));
         }
 
-        private static void TestLoginDetails(Sage50LoginDetails loginDetails)
+        private static SageConnectionFactory CreateConnectionFactory(Sage50LoginDetails loginDetails)
+        {
+            var connectionString = CreateConnectionString(loginDetails);
+            var factory = new SageConnectionFactory(connectionString);
+            TestFactory(factory);
+            return factory;
+        }
+
+        private static void TestFactory(SageConnectionFactory loginDetails)
         {
             using (var connection = loginDetails.OpenConnection())
             {
                 connection.GetSchema();
             }
+        }
+
+        private static string CreateConnectionString(Sage50LoginDetails loginDetails)
+        {
+            var builder = new OdbcConnectionStringBuilder
+            {
+                Driver = "Sage Line 50 v21"
+            };
+
+            builder["uid"] = loginDetails.Username;
+            builder["dir"] = loginDetails.DataDirectory;
+
+            if (!string.IsNullOrEmpty(loginDetails.Password))
+            {
+                builder["pwd"] = loginDetails.Password;
+            }
+
+            var connectionString = builder.ConnectionString;
+            return connectionString;
         }
     }
 }
