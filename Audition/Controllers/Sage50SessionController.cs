2a9a48ff2e70281407541cae4b260a51b30eed51
hairybreeches <hairybreeches@bitbucket.org>
2014-10-09 11:49:47 +0100

    routing and data submission for sage 50 login

---
+++ Audition/Controllers/Sage50SessionController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Http;
+using Audition.Chromium;
+using Audition.Session;
+using Sage50;
+
+namespace Audition.Controllers
+{
+    public class Sage50SessionController : RedirectController
+    {
+        private readonly LoginSession session;
+
+        public Sage50SessionController(LoginSession session)
+        {
+            this.session = session;
+        }
+
+        [HttpPost]
+        [Route(Routing.Sage50Login)]
+        public IHttpActionResult Login(Sage50LoginDetails loginDetails)
+        {            
+            return RedirectToView("search.html");
+        }               
+
+        [HttpGet]
+        [Route(Routing.Sage50Logout)]
+        public IHttpActionResult Logout()
+        {
+            session.Logout();
+            return RedirectToView("login.html");
+        }
+    }
+}
