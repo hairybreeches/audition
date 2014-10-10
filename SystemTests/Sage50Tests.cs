c720c6040df0767b704b1c2632958f3ead57ba49
hairybreeches <hairybreeches@bitbucket.org>
2014-10-10 15:37:12 +0100

    usings

--- SystemTests/Sage50Tests.cs
+++ SystemTests/Sage50Tests.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.Odbc;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Model.Accounting;
-using Model.SearchWindows;
 using NUnit.Framework;
 using Sage50;
 
