787b860cbf91a90fbe05567ded508b49b8c34c2d
hairybreeches <hairybreeches@bitbucket.org>
2014-10-10 14:35:05 +0100

    reduce duplication again

---
+++ Sage50/SchemaColumn.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data;
+using Sage50.Parsing;
+
+namespace Sage50
+{
+    class SchemaColumn<T> : ISchemaColumn
+    {
+        private readonly int index;
+        private readonly string fieldName;
+
+        public SchemaColumn(string fieldName, int index)
+        {
+            this.fieldName = fieldName;
+            this.index = index;
+        }
+
+        public int Index
+        {
+            get { return index; }
+        }
+
+        public string FieldName
+        {
+            get { return fieldName; }
+        }
+
+        public DataColumn ToDataColumn()
+        {
+            return new DataColumn(FieldName, typeof(T));
+        }
+
+        public T GetField(IDataRecord record)
+        {
+            var actualFieldName = record.GetName(Index);
+            if (actualFieldName != FieldName)
+            {
+                throw new SageDataFormatUnexpectedException(
+                    String.Format("Unrecognised data schema. Column {0} was {1}, expected {2}", Index,
+                        actualFieldName, FieldName));
+            }
+
+            var fieldValue = record[Index];
+
+            if (!(fieldValue is T))
+            {
+                throw new SageDataFormatUnexpectedException(
+                    String.Format("Unrecognised data schema. {0} was {1}, expected {2}", FieldName,
+                        fieldValue.GetType(), typeof(T)));
+            }
+
+            return (T)fieldValue;
+        }
+    }
+}
