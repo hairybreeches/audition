1e41bb8a23892dd1ac1bd8379794941edd4b94b7
hairybreeches <hairybreeches@bitbucket.org>
2014-10-10 10:52:17 +0100

    extract all these static methods into a separate class

---
+++ SystemTests/SystemFoo.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using Audition;
+using Audition.Chromium;
+using Audition.Controllers;
+using Autofac;
+using CefSharp;
+using Newtonsoft.Json;
+using Xero;
+
+namespace SystemTests
+{
+    public static class SystemFoo
+    {
+        public static string MungeJson(string value)
+        {           
+            return JsonConvert.SerializeObject(JsonConvert.DeserializeObject(value));           
+        }
+
+        public static ContainerBuilder CreateDefaultContainerBuilder()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<AuditionModule>();
+            return builder;
+        }
+
+        public static string GetResponseContent(ContainerBuilder builder, MockRequestResponse requestResponse)
+        {
+            var cefSharpResponse = ExecuteRequest(builder, requestResponse);
+
+            string actual;
+            using (var reader = new StreamReader(cefSharpResponse.Content))
+            {
+                actual = reader.ReadToEnd();
+            }
+            return actual;
+        }
+
+        public static CefSharpResponse ExecuteRequest(ContainerBuilder builder, MockRequestResponse requestResponse)
+        {
+            CefSharpResponse cefSharpResponse;
+            using (var lifetime = builder.Build())
+            {
+                LoginToXero(lifetime);
+                var handler = lifetime.Resolve<IRequestHandler>();
+
+
+                handler.OnBeforeResourceLoad(null, requestResponse);
+
+                cefSharpResponse = requestResponse.Response;
+            }
+            return cefSharpResponse;
+        }
+
+        private static void LoginToXero(IComponentContext lifetime)
+        {
+            var loginController = lifetime.Resolve<XeroSessionController>();
+            loginController.PostCompleteAuthenticationRequest(new XeroVerificationCode());
+        }
+    }
+}
