1dc373a5edbd5e71fb92b104a621031828e7094c
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-10-10 11:02:57 +0100

    worked out where some of these methods belong

--- SystemTests/SystemFoo.cs
+++ SystemTests/SystemFoo.cs
@@ -1,11 +1,6 @@
-﻿using System.IO;
-using Audition;
-using Audition.Chromium;
-using Audition.Controllers;
+﻿using Audition;
 using Autofac;
-using CefSharp;
 using Newtonsoft.Json;
-using Xero;
 
 namespace SystemTests
 {
@@ -21,28 +16,6 @@ namespace SystemTests
             var builder = new ContainerBuilder();
             builder.RegisterModule<AuditionModule>();
             return builder;
-        }
-
-        public static string GetResponseContent(IComponentContext lifetime, MockRequestResponse requestResponse)
-        {
-            var cefSharpResponse = ExecuteRequest(lifetime, requestResponse);            
-            using (var reader = new StreamReader(cefSharpResponse.Content))
-            {
-                return reader.ReadToEnd();
-            }
-        }
-
-        public static CefSharpResponse ExecuteRequest(IComponentContext lifetime, MockRequestResponse requestResponse)
-        {
-            var handler = lifetime.Resolve<IRequestHandler>();
-            handler.OnBeforeResourceLoad(null, requestResponse);
-            return requestResponse.Response;
-        }
-
-        public static void LoginToXero(IComponentContext lifetime, XeroVerificationCode verificationCode)
-        {
-            var loginController = lifetime.Resolve<XeroSessionController>();
-            loginController.PostCompleteAuthenticationRequest(verificationCode);
         }
     }
 }
