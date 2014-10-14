b33e907e44e3dc4f27bb8a6d58726c58c0daa9f5
hairybreeches <hairybreeches@bitbucket.org>
2014-10-14 14:55:15 +0100

    add an export request class with more information

--- Model/SearchWindows/KeywordParameters.cs
+++ Model/SearchWindows/KeywordParameters.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Model.SearchWindows
 {
@@ -9,6 +10,24 @@ namespace Model.SearchWindows
         public KeywordParameters(string keywords)
         {
             Keywords = InputParsing.ParseStringList(keywords);
+        }
+
+        protected bool Equals(KeywordParameters other)
+        {
+            return Keywords.SequenceEqual(other.Keywords);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((KeywordParameters) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Keywords != null ? Keywords.GetHashCode() : 0);
         }
     }
 }
