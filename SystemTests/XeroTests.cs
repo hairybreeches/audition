807ea6f6e761b3fdc8ad1467e4dd8b0a79b0b12e
hairybreeches <hairybreeches@bitbucket.org>
2014-10-15 16:40:26 +0100

    test more of the stack: get rid of mocking method

--- SystemTests/XeroTests.cs
+++ SystemTests/XeroTests.cs
@@ -11,39 +11,55 @@ using NSubstitute;
 using NUnit.Framework;
 using Tests.Mocks;
 using Xero;
+using XeroApi.Model;
+using Journal = Model.Accounting.Journal;
+using JournalLine = Model.Accounting.JournalLine;
 
 namespace SystemTests
 {
     [TestFixture]
     public class XeroTests
     {
-        private readonly IRepositoryFactory repositoryFactory = Mock.RepositoryFactory(
-            new Journal(new Guid("0421c274-2f50-49e4-8f61-623a4daf67ac"), new DateTime(2013, 4, 6), new DateTime(2013, 4, 6),
-                new List<JournalLine>
-                {
+        private readonly IXeroSession xeroSession = new MockXeroSession(
+            CreateXeroJournal(new Guid("0421c274-2f50-49e4-8f61-623a4daf67ac"), new DateTime(2013, 4, 6), new DateTime(2013, 4, 6), 
                     new JournalLine("9012", "Expenses", JournalType.Cr, 23.4m),
-                    new JournalLine("3001", "Cash", JournalType.Dr, 23.4m)
-                }),
-            new Journal(new Guid("c8d99cf8-6867-4767-be1e-abdf54a2a0f8"), new DateTime(2013, 4, 6), new DateTime(2013, 4, 6),
-                new List<JournalLine>
-                {
+                    new JournalLine("3001", "Cash", JournalType.Dr, 23.4m)),
+
+            CreateXeroJournal(new Guid("c8d99cf8-6867-4767-be1e-abdf54a2a0f8"), new DateTime(2013, 4, 6), new DateTime(2013, 4, 6),                
                     new JournalLine("8014", "Depreciation", JournalType.Cr, 12.4m),
                     new JournalLine("4001", "Fixed assets", JournalType.Dr, 12.4m)
-                }),
+                ),
             //this one is outside the financial period
-            new Journal(Guid.NewGuid(), new DateTime(2012, 6, 5), new DateTime(2012, 6, 5),
-                new List<JournalLine>
-                {
+            CreateXeroJournal(Guid.NewGuid(), new DateTime(2012, 6, 5), new DateTime(2012, 6, 5),               
                     new JournalLine("8014", "Depreciation", JournalType.Cr, 12.4m),
                     new JournalLine("4001", "Fixed assets", JournalType.Dr, 12.4m)
-                }),
+                ),
             //this one is "boring"
-            new Journal(Guid.NewGuid(), new DateTime(2014, 4, 2, 12, 0, 0), new DateTime(2013, 6, 5),
-                new List<JournalLine>
-                {
+            CreateXeroJournal(Guid.NewGuid(), new DateTime(2014, 4, 2, 12, 0, 0), new DateTime(2013, 6, 5),               
                     new JournalLine("8014", "Depreciation", JournalType.Cr, 12.4m),
                     new JournalLine("4001", "Fixed assets", JournalType.Dr, 12.4m)
-                }));
+                ));
+
+        private static XeroApi.Model.Journal CreateXeroJournal(Guid journalId, DateTime createdDateUtc, DateTime journalDate, params JournalLine[] lines)
+        {
+            var xeroJournal = new XeroApi.Model.Journal
+            {
+                JournalID = journalId,
+                CreatedDateUTC = createdDateUtc,
+                JournalDate = journalDate,
+                JournalLines = new JournalLines()
+            };
+            foreach (var line in lines)
+            {
+                xeroJournal.JournalLines.Add(new XeroApi.Model.JournalLine()
+                {
+                    AccountCode = line.AccountCode,
+                    AccountName = line.AccountName,
+                    NetAmount = line.Amount * (line.JournalType == JournalType.Dr? 1 : -1)
+                });
+            }
+            return xeroJournal;
+        }
 
         private const string SearchWindow =
             "{'Period':{'From':'2013-4-5','To':'2014-4-4'},'Parameters':{'FromDay':'Monday','ToDay':'Friday','FromTime':'08:00','ToTime':'18:00'}}";
@@ -79,8 +95,8 @@ namespace SystemTests
 
             StringAssert.AreEqualIgnoringCase(
                 @"Created,Date,Username
-06/04/2013 00:00:00 +01:00,06/04/2013 00:00:00,,Cr,9012,Expenses,23.4,Dr,3001,Cash,23.4
-06/04/2013 00:00:00 +01:00,06/04/2013 00:00:00,,Cr,8014,Depreciation,12.4,Dr,4001,Fixed assets,12.4
+06/04/2013 01:00:00 +01:00,06/04/2013 00:00:00,,Cr,9012,Expenses,23.4,Dr,3001,Cash,23.4
+06/04/2013 01:00:00 +01:00,06/04/2013 00:00:00,,Cr,8014,Depreciation,12.4,Dr,4001,Fixed assets,12.4
 ", fileContents);
             
         }        
@@ -96,7 +112,7 @@ namespace SystemTests
 @"[
     {
         'Id':'0421c274-2f50-49e4-8f61-623a4daf67ac',
-        'Created':'2013-04-06T00:00:00+01:00',
+        'Created':'2013-04-06T01:00:00+01:00',
         'JournalDate':'2013-04-06T00:00:00',
         'Username':'',
         'Description':'',
@@ -107,7 +123,7 @@ namespace SystemTests
     },
     {
         'Id':'c8d99cf8-6867-4767-be1e-abdf54a2a0f8',
-        'Created':'2013-04-06T00:00:00+01:00',
+        'Created':'2013-04-06T01:00:00+01:00',
         'JournalDate':'2013-04-06T00:00:00',
         'Username':'',
         'Description':'',
@@ -126,7 +142,7 @@ namespace SystemTests
         private ContainerBuilder CreateContainerBuilder()
         {
             var builder = SystemFoo.CreateDefaultContainerBuilder();
-            builder.Register(_ => repositoryFactory).As<IRepositoryFactory>();            
+            builder.Register(_ => xeroSession).As<IXeroSession>();            
             return builder;
         }
 
