2cf20af442c096a6b562692ca7ffc0b64c1db056
hairybreeches <hairybreeches@bitbucket.org>
2014-10-16 13:41:24 +0100

    throw an exception to let people know when xero accounts are too big

--- Xero/XeroSlurper.cs
+++ Xero/XeroSlurper.cs
@@ -7,19 +7,28 @@ namespace Xero
 {
     internal class XeroSlurper
     {
+        private const int MaxResultsPerRequest = 100;
+        private const int MaxRequests = 30;
+        private const int MaxTotalResults = MaxResultsPerRequest*MaxRequests;
+
         public async Task<IEnumerable<Journal>> Slurp(IXeroJournalSource repository)
         {
             var lastTaken = 0;
             var journals = new List<XeroApi.Model.Journal>();
-            for (var i = 0; i < 30; i++)
+            for (var i = 0; i < MaxRequests; i++)
             {
                 
                 journals.AddRange(await GetBatch(repository, lastTaken));
                 
-                if (journals.Count < 100)
+                if (journals.Count < MaxResultsPerRequest)
                     break;
 
                 lastTaken = (int) journals.Last().JournalNumber;
+            }
+
+            if (journals.Count >= MaxTotalResults)
+            {
+                throw new TooManyJournalsException(MaxTotalResults);
             }
 
             return journals.Select(x=>x.ToModelJournal());
