fce891adef908cbf284bc23492fd662e23e2508f
hairybreeches <hairybreeches@bitbucket.org>
2014-10-23 15:31:56 +0100

    better error messages for the cases where user fails to login

--- Sage50/Sage50SearcherFactory.cs
+++ Sage50/Sage50SearcherFactory.cs
@@ -1,5 +1,6 @@
-﻿using Model.Searching;
-using Model.SearchWindows;
+﻿using System.Data.Odbc;
+using Model;
+using Model.Searching;
 
 namespace Sage50
 {
@@ -14,14 +15,30 @@ namespace Sage50
 
         public IJournalSearcher CreateJournalSearcher(Sage50LoginDetails loginDetails)
         {
-            var repository = repositoryFactory.CreateJournalRepository(loginDetails);
+            try
+            {
+                var repository = repositoryFactory.CreateJournalRepository(loginDetails);
+                return new JournalSearcher(
+                    new WorkingHoursSearcher(repository),
+                    new YearEndSearcher(repository),
+                    new UnusualAccountsSearcher(repository),
+                    new RoundNumberSearcher(repository),
+                    new UserSearcher(repository));
+            }
+            catch (OdbcException e)
+            {
+                var error = e.Errors[0];
+                if (error.SQLState == "08001")
+                {
+                    throw new IncorrectLoginDetailsException("The specified folder does not appear to be a Sage 50 data directory. The data directory can be found by logging in to Sage and clicking help->about from the menu.");
+                }
+                if (error.SQLState == "28000")
+                {
+                    throw new IncorrectLoginDetailsException("Incorrect username or password");
+                }
 
-            return new JournalSearcher(
-                new WorkingHoursSearcher(repository),
-                new YearEndSearcher(repository),
-                new UnusualAccountsSearcher(repository),
-                new RoundNumberSearcher(repository),
-                new UserSearcher(repository));
+                throw;
+            }
         }
 
         
