f7a9f1fcdb3c515c5f099eaf7f6fbeaf68662c29
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-10-24 10:59:32 +0100

    if we get any mangled driver names, ignore them

--- Tests/DriverDetectorTests.cs
+++ Tests/DriverDetectorTests.cs
@@ -22,7 +22,11 @@ namespace Tests
                     .SetName("Version numbers are sorted as ints, not strings"); 
                                           
                 yield return new TestCaseData(new List<string> {"Sage Line 50 v26", "Sage Line 50 v11", "SQL Server"}).Returns(new Sage50Driver(26, "Sage Line 50 v26"))
-                    .SetName("Non-Sage drivers are ignored");
+                    .SetName("Non-Sage drivers are ignored");    
+                
+                
+                yield return new TestCaseData(new List<string> {"Sage Line 50 v26", "Sage Line 50 vichysoisse add-in", "Sage Line 50 v11", "SQL Server"}).Returns(new Sage50Driver(26, "Sage Line 50 v26"))
+                    .SetName("Unparseable 'Sage' drivers are ignored");
                 
             }
         }        
