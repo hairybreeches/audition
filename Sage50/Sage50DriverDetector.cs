7cd991280d25e12ea440de87c7ca8dfc50648ef0
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-10-24 10:23:22 +0100

    extract registry reading to be mockable

--- Sage50/Sage50DriverDetector.cs
+++ Sage50/Sage50DriverDetector.cs
@@ -1,31 +1,23 @@
-﻿using System.Collections.Generic;
-using System.Globalization;
+﻿using System.Globalization;
 using System.Linq;
-using Microsoft.Win32;
 
 namespace Sage50
 {
     public class Sage50DriverDetector
     {
+        private readonly IRegistryReader registryReader;
+
+        public Sage50DriverDetector(IRegistryReader registryReader)
+        {
+            this.registryReader = registryReader;
+        }
+
         public Sage50Driver FindBestDriver()
         {
-            var driverNames = DriverNames();
+            var driverNames = registryReader.Get32BitOdbcDrivers();
             var sageDrivers = driverNames.Where(x => x.StartsWith("Sage Line 50", true, CultureInfo.CurrentCulture));
             var sortedSageDrivers = sageDrivers.Select(Sage50Driver.Create).OrderByDescending(x => x.Version);            
             return sortedSageDrivers.First();
-        }
-
-        private static IEnumerable<string> DriverNames()
-        {
-            var driversKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32)
-                .OpenSubKey("SOFTWARE\\ODBC\\ODBCINST.INI\\ODBC Drivers");
-
-            if (driversKey == null)
-            {
-                throw new SageNotInstalledException();
-            }
-
-            return driversKey.GetValueNames();
         }
     }
 }
