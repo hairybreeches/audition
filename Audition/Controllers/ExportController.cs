9ff3d0d927b34f6844db2edff72cafe2508160bb
hairybreeches <hairybreeches@bitbucket.org>
2014-10-24 16:00:31 +0100

    split the search/export functionality

---
+++ Audition/Controllers/ExportController.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Audition.Chromium;
+using Audition.Native;
+using Audition.Requests;
+using Audition.Session;
+using Excel;
+using Model;
+using Model.Accounting;
+using Model.Searching;
+using Model.SearchWindows;
+using Model.Time;
+
+namespace Audition.Controllers
+{
+    public class ExportController : ApiController
+    {
+        private readonly LoginSession session;
+        private readonly ExcelExporter excelExporter;
+        private readonly IFileSaveChooser fileSaveChooser;
+
+        private IJournalSearcher Searcher
+        {
+            get { return session.GetCurrentJournalSearcher(); }
+        }
+
+        public ExportController(IFileSaveChooser fileSaveChooser, ExcelExporter excelExporter, LoginSession session)
+        {
+            this.fileSaveChooser = fileSaveChooser;
+            this.excelExporter = excelExporter;
+            this.session = session;
+        }
+
+        [HttpPost]
+        [Route(Routing.HoursExport)]
+        public async Task<IHttpActionResult> HoursExport(ExportRequest<WorkingHours> saveRequest)
+        {
+            var journals = Searcher.FindJournalsWithin(saveRequest.SearchWindow);
+            return await Export(journals, saveRequest.SerialisationOptions);
+        }
+
+        [HttpPost]
+        [Route(Routing.AccountsExport)]
+        public async Task<IHttpActionResult> AccountsExport(ExportRequest<UnusualAccountsParameters> saveRequest)
+        {
+            var journals = Searcher.FindJournalsWithin(saveRequest.SearchWindow);
+            return await Export(journals, saveRequest.SerialisationOptions);
+        }
+
+        [HttpPost]
+        [Route(Routing.DateExport)]
+        public async Task<IHttpActionResult> DateExport(ExportRequest<YearEndParameters> saveRequest)
+        {
+            var journals = Searcher.FindJournalsWithin(saveRequest.SearchWindow);
+            return await Export(journals, saveRequest.SerialisationOptions);
+        }
+
+        [HttpPost]
+        [Route(Routing.EndingExport)]
+        public async Task<IHttpActionResult> EndingExport(ExportRequest<EndingParameters> saveRequest)
+        {
+            var journals = Searcher.FindJournalsWithin(saveRequest.SearchWindow);
+            return await Export(journals, saveRequest.SerialisationOptions);
+        }
+
+        [HttpPost]
+        [Route(Routing.UserExport)]
+        public async Task<IHttpActionResult> UserExport(ExportRequest<UserParameters> saveRequest)
+        {
+            var journals = Searcher.FindJournalsWithin(saveRequest.SearchWindow);
+            return await Export(journals, saveRequest.SerialisationOptions);
+        }
+
+        private async Task<IHttpActionResult> Export(IEnumerable<Journal> journals, SerialisationOptions options)
+        {
+            var saveLocation = await fileSaveChooser.GetFileSaveLocation();
+            excelExporter.WriteJournals(journals, saveLocation, options);
+            return Ok(saveLocation);
+        }
+    }
+}
--- Audition/Controllers/SearchController.cs
+++ Audition/Controllers/SearchController.cs
@@ -1,12 +1,7 @@
 ﻿using System.Collections.Generic;
-using System.Threading.Tasks;
 using System.Web.Http;
 using Audition.Chromium;
-using Audition.Native;
-using Audition.Requests;
 using Audition.Session;
-using Excel;
-using Model;
 using Model.Accounting;
 using Model.Searching;
 using Model.SearchWindows;
@@ -20,16 +15,12 @@ namespace Audition.Controllers
         {
             get { return session.GetCurrentJournalSearcher(); }
         }
-
-        private readonly ExcelExporter excelExporter;
-        private readonly IFileSaveChooser fileSaveChooser;
+        
         private readonly LoginSession session;
 
-        public SearchController(LoginSession session, ExcelExporter excelExporter, IFileSaveChooser fileSaveChooser)
+        public SearchController(LoginSession session)
         {
             this.session = session;
-            this.excelExporter = excelExporter;
-            this.fileSaveChooser = fileSaveChooser;
         }
 
         [HttpPost]
@@ -40,14 +31,6 @@ namespace Audition.Controllers
         }
         
         [HttpPost]
-        [Route(Routing.HoursExport)]
-        public async Task<IHttpActionResult> HoursExport(ExportRequest<WorkingHours> saveRequest)
-        {
-            var journals = Searcher.FindJournalsWithin(saveRequest.SearchWindow);
-            return await Export(journals, saveRequest.SerialisationOptions);
-        }      
-        
-        [HttpPost]
         [Route(Routing.AccountsSearch)]
         public IEnumerable<Journal> AccountsSearch(SearchWindow<UnusualAccountsParameters> searchWindow)
         {
@@ -55,26 +38,10 @@ namespace Audition.Controllers
         }
         
         [HttpPost]
-        [Route(Routing.AccountsExport)]
-        public async Task<IHttpActionResult> AccountsExport(ExportRequest<UnusualAccountsParameters> saveRequest)
-        {
-            var journals = Searcher.FindJournalsWithin(saveRequest.SearchWindow);
-            return await Export(journals, saveRequest.SerialisationOptions);
-        }    
-        
-        [HttpPost]
         [Route(Routing.DateSearch)]
         public IEnumerable<Journal> DateSearch(SearchWindow<YearEndParameters> searchWindow)
         {
             return Searcher.FindJournalsWithin(searchWindow);
-        }
-        
-        [HttpPost]
-        [Route(Routing.DateExport)]
-        public async Task<IHttpActionResult> DateExport(ExportRequest<YearEndParameters> saveRequest)
-        {
-            var journals = Searcher.FindJournalsWithin(saveRequest.SearchWindow);
-            return await Export(journals, saveRequest.SerialisationOptions);
         }
 
         [HttpPost]
@@ -85,33 +52,10 @@ namespace Audition.Controllers
         }
         
         [HttpPost]
-        [Route(Routing.UserExport)]
-        public async Task<IHttpActionResult> UserExport(ExportRequest<UserParameters> saveRequest)
-        {
-            var journals = Searcher.FindJournalsWithin(saveRequest.SearchWindow);
-            return await Export(journals, saveRequest.SerialisationOptions);
-        }
-        
-        [HttpPost]
         [Route(Routing.EndingSearch)]
         public IEnumerable<Journal> EndingSearch(SearchWindow<EndingParameters> searchWindow)
         {
             return Searcher.FindJournalsWithin(searchWindow);
-        }
-        
-        [HttpPost]
-        [Route(Routing.EndingExport)]
-        public async Task<IHttpActionResult> EndingExport(ExportRequest<EndingParameters> saveRequest)
-        {
-            var journals = Searcher.FindJournalsWithin(saveRequest.SearchWindow);
-            return await Export(journals, saveRequest.SerialisationOptions);
-        }
-
-        private async Task<IHttpActionResult> Export(IEnumerable<Journal> journals, SerialisationOptions options)
-        {
-            var saveLocation = await fileSaveChooser.GetFileSaveLocation();
-            excelExporter.WriteJournals(journals, saveLocation, options);
-            return Ok(saveLocation);
         }
     }
 }
