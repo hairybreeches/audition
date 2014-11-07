17ec96a6b0cbcb2a118fb7ba24be739e82cff2d1
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-11-07 15:26:37 +0000

    test to make sure the data not evaluated outside the repository

---
+++ Tests/MemoryUsageTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.Odbc;
+using Audition.Controllers;
+using Autofac;
+using NSubstitute;
+using NUnit.Framework;
+using Persistence;
+using Sage50;
+using Sage50.Parsing;
+using Tests.Mocks;
+
+namespace Tests
+{
+    [TestFixture]
+    public class MemoryUsageTests
+    {
+        [Test]
+        public void JournalEnumerableNotEvaluatedOutsideRepository()
+        {
+            Assert.DoesNotThrow(() => LoginAndAddJournals(Substitute.For<IJournalRepository>()));
+        }    
+        
+        [Test]
+        public void JournalEnumerableEvaluatedInsideRepository()
+        {
+            Assert.Throws<EnumerableEvaluatedException>(() => LoginAndAddJournals(new InMemoryJournalRepository()));
+        }
+
+        private static void LoginAndAddJournals(IJournalRepository journalRepository)
+        {
+            var builder = AutofacConfiguration.CreateDefaultContainerBuilder();
+            builder.RegisterType<ConnectionFactoryWhichThrowsWhenReaderIsEvaluated>().As<ISage50ConnectionFactory>();
+            builder.Register(_ => journalRepository).As<IJournalRepository>();
+            builder.Register(_ => Substitute.For<INominalCodeLookupFactory>()).As<INominalCodeLookupFactory>();
+
+            using (var lifetime = builder.Build())
+            using (var request = lifetime.BeginRequestScope())
+            {
+                var controller = request.Resolve<Sage50SessionController>();
+                controller.Login(new Sage50LoginDetails());
+            }
+        }
+    }
+}
