fbdad0efd1ea1ce493f2aec3efb6cc954f379979
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-11-07 15:21:11 +0000

    and mockable

--- Sage50/Sage50Module.cs
+++ Sage50/Sage50Module.cs
@@ -13,7 +13,7 @@ namespace Sage50
             builder.RegisterType<JournalReader>();
             builder.RegisterType<JournalLineParser>();
             builder.RegisterType<Sage50JournalGetter>();
-            builder.RegisterType<NominalCodeLookupFactory>();
+            builder.RegisterType<NominalCodeLookupFactory>().As<INominalCodeLookupFactory>();
             builder.RegisterType<Sage50ConnectionFactory>().As<ISage50ConnectionFactory>();
             builder.RegisterType<Sage50DriverDetector>();
             builder.RegisterType<RegistryReader>().As<IRegistryReader>();
