6c24abde305ff4a96257bc2d66a31bb3cc9145a3
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-11-10 17:01:22 +0000

    pagesize is now encapsulated here

--- Persistence/JournalQueryableExtensions.cs
+++ Persistence/JournalQueryableExtensions.cs
@@ -9,21 +9,23 @@ namespace Persistence
 {
     public static class JournalQueryableExtensions
     {
+        private const int Pagesize = 10;
+
         public static SearchResponse GetPage(this IQueryable<Journal> journals, int pageNumber)
         {
             var listOfAllJournals = journals.ToList();
             var totalResults = listOfAllJournals.Count();
-            var totalPages = Math.Ceiling((double) totalResults / Constants.Pagesize);
+            var totalPages = Math.Ceiling((double) totalResults / Pagesize);
 
             if (pageNumber > totalPages || pageNumber < 1)
             {
                 throw new InvalidPageNumberException("Cannot go to page " + pageNumber + ". There are " + totalPages + " pages.");
             }
 
-            var firstResult = (Constants.Pagesize*(pageNumber - 1)) + 1;
+            var firstResult = (Pagesize*(pageNumber - 1)) + 1;
 
-            var numberOfResultsToSkip = (pageNumber - 1) * Constants.Pagesize;
-            var journalsToReturn = listOfAllJournals.Skip(numberOfResultsToSkip).Take(Constants.Pagesize).ToList();
+            var numberOfResultsToSkip = (pageNumber - 1) * Pagesize;
+            var journalsToReturn = listOfAllJournals.Skip(numberOfResultsToSkip).Take(Pagesize).ToList();
             var isNextPage = pageNumber < totalPages;
             var isPreviousPage = pageNumber > 1;
             return new SearchResponse(journalsToReturn, totalResults.ToString(),  isPreviousPage, isNextPage, firstResult);
@@ -32,6 +34,6 @@ namespace Persistence
         public static IEnumerable<Journal> GetAllJournals(this IQueryable<Journal> journals)
         {
             return journals;
-        }
+        }        
     }
 }
