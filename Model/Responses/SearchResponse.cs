52dca1d2c08d689bcc65a42710a4e2442e5cd3c5
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-11-10 16:47:20 +0000

    do a bit more of the pagination logic server side

--- Model/Responses/SearchResponse.cs
+++ Model/Responses/SearchResponse.cs
@@ -5,13 +5,19 @@ namespace Model.Responses
 {
     public class SearchResponse
     {
-        public SearchResponse(IList<Journal> journals, int totalResults)
+        public SearchResponse(IList<Journal> journals, int totalResults, bool isPreviousPage, bool isNextPage, int firstResult)
         {
+            FirstResult = firstResult;
+            IsNextPage = isNextPage;
+            IsPreviousPage = isPreviousPage;
             TotalResults = totalResults;
             Journals = journals;
         }
 
         public IList<Journal> Journals { get; private set; } 
-        public int TotalResults { get; private set; } 
+        public int TotalResults { get; private set; }
+        public bool IsPreviousPage { get; private set; }
+        public bool IsNextPage { get; private set; }        
+        public int FirstResult { get; private set; }        
     }
 }
