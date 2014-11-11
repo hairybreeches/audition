63a1f1a8607a40d7e183e5b928aafaa994843ab5
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-11-11 10:48:19 +0000

    and test to make sure the correct page has indeed been returned

--- Tests/SearcherTests/PaginationTests.cs
+++ Tests/SearcherTests/PaginationTests.cs
@@ -37,7 +37,7 @@ namespace Tests.SearcherTests
             {
                 requestScope.Resolve<ExportController>().EndingExport(requestData).Wait();
                 var ids = exporter.WrittenJournals.Select(x => x.Id);
-                CollectionAssert.AreEqual(Enumerable.Range(0, 3000).Select(x => x.ToString()), ids);
+                CollectionAssert.AreEqual(Enumerable.Range(1, 3000).Select(x => x.ToString()), ids);
             }           
         }
 
@@ -46,51 +46,51 @@ namespace Tests.SearcherTests
         public void PaginationWorksForEndingSearch()
         {
             var journals = Searching.ExecuteSearch(CreateSearchRequest(new EndingParameters(0)), GetJournals());
-            CollectionAssert.AreEqual(Enumerable.Range(1480, 10).Select(x => x.ToString()), journals.Select(x=>x.Id));
+            CollectionAssert.AreEqual(Enumerable.Range(1481, 10).Select(x => x.ToString()), journals.Select(x=>x.Id));
         }
         
         [Test]
         public void PaginationWorksForAccountsSearch()
         {
             var journals = Searching.ExecuteSearch(CreateSearchRequest(new UnusualAccountsParameters(1000000000)), GetJournals());
-            CollectionAssert.AreEqual(Enumerable.Range(1480, 10).Select(x => x.ToString()), journals.Select(x=>x.Id));
+            CollectionAssert.AreEqual(Enumerable.Range(1481, 10).Select(x => x.ToString()), journals.Select(x=>x.Id));
         }
         
         [Test]
         public void PaginationWorksForYearEndSearch()
         {
             var journals = Searching.ExecuteSearch(CreateSearchRequest(new YearEndParameters((DateTime.MaxValue - DateTime.MinValue).Days)), GetJournals());
-            CollectionAssert.AreEqual(Enumerable.Range(1480, 10).Select(x => x.ToString()), journals.Select(x=>x.Id));
+            CollectionAssert.AreEqual(Enumerable.Range(1481, 10).Select(x => x.ToString()), journals.Select(x=>x.Id));
         }
         
         [Test]
         public void PaginationWorksForUserSearch()
         {
             var journals = Searching.ExecuteSearch(CreateSearchRequest(new UserParameters("a non-existent user")), GetJournals());
-            CollectionAssert.AreEqual(Enumerable.Range(1480, 10).Select(x => x.ToString()), journals.Select(x=>x.Id));
+            CollectionAssert.AreEqual(Enumerable.Range(1481, 10).Select(x => x.ToString()), journals.Select(x=>x.Id));
         }
         
         [Test]
         public void PaginationWorksForWorkingHoursSearch()
         {
             var journals = Searching.ExecuteSearch(CreateSearchRequest(new WorkingHoursParameters(DayOfWeek.Monday, DayOfWeek.Monday, new LocalTime(7, 32), new LocalTime(7, 32))), GetJournals());
-            CollectionAssert.AreEqual(Enumerable.Range(1480, 10).Select(x => x.ToString()), journals.Select(x=>x.Id));
+            CollectionAssert.AreEqual(Enumerable.Range(1481, 10).Select(x => x.ToString()), journals.Select(x=>x.Id));
         }    
                 
         
-        [TestCase(15, 0, true, true, "15",ExpectedException = typeof(InvalidPageNumberException), TestName = "Requesting a page number < 1 gives correct exception")]
-        [TestCase(15, 3, true, true, "15", ExpectedException = typeof(InvalidPageNumberException), TestName = "Requesting a page number too large gives correct exception")]
-        [TestCase(6, 1, false, false, "6", TestName = "Less than one page of results means the only page has no next or previous")]
-        [TestCase(10, 1, false, false, "10", TestName = "Exactly one page of results means the only page has no next or previous")]
-        [TestCase(40, 2, true, true, "40", TestName = "A page in the middle should have a next and a previous page")]
-        [TestCase(40, 4, true, false, "40", TestName = "A full last page should have a previous but no next")]
-        [TestCase(36, 4, true, false, "36", TestName = "A half-full last page should have a previous but no next")]
-        [TestCase(2560, 1, false, true, "2560", TestName = "First page of large result set displays correct results string")]
-        [TestCase(2560, 5, true, true, "2560", TestName = "Early middle page of large result set displays correct results string")]
-        [TestCase(2560, 255, true, true, "2560", TestName = "Second last page of large result set displays correct results string")]
-        [TestCase(2560, 256, true, false, "2560", TestName = "A full last page on a large results set should show correct results string")]
-        [TestCase(2556, 256, true, false, "2556", TestName = "A half-full last page on a large results set should show correct results string")]
-        public void NextAndPreviousButtonsAvailableWhenAppropriate(int numberOfResults, int pageNumber, bool previousPageShouldBeAvailable, bool nextPageShouldBeAvailable, string totalResults)
+        [TestCase(15, 0, true, true, "15", -1, 0, ExpectedException = typeof(InvalidPageNumberException), TestName = "Requesting a page number < 1 gives correct exception")]
+        [TestCase(15, 3, true, true, "15", -1,0, ExpectedException = typeof(InvalidPageNumberException), TestName = "Requesting a page number too large gives correct exception")]
+        [TestCase(6, 1, false, false, "6", 1,6, TestName = "Less than one page of results means the only page has no next or previous")]
+        [TestCase(10, 1, false, false, "10", 1,10, TestName = "Exactly one page of results means the only page has no next or previous")]
+        [TestCase(40, 2, true, true, "40", 11,10, TestName = "A page in the middle should have a next and a previous page")]
+        [TestCase(40, 4, true, false, "40", 31,10, TestName = "A full last page should have a previous but no next")]
+        [TestCase(36, 4, true, false, "36", 31,6, TestName = "A half-full last page should have a previous but no next")]
+        [TestCase(2560, 1, false, true, "2560", 1,10, TestName = "First page of large result set displays correct results string")]
+        [TestCase(2560, 5, true, true, "2560", 41,10, TestName = "Early middle page of large result set displays correct results string")]
+        [TestCase(2560, 255, true, true, "2560",2541,10,TestName = "Second last page of large result set displays correct results string")]
+        [TestCase(2560, 256, true, false, "2560",2551,10, TestName = "A full last page on a large results set should show correct results string")]
+        [TestCase(2556, 256, true, false, "2556",2551,6, TestName = "A half-full last page on a large results set should show correct results string")]
+        public void NextAndPreviousButtonsAvailableWhenAppropriate(int numberOfResults, int pageNumber, bool previousPageShouldBeAvailable, bool nextPageShouldBeAvailable, string totalResults, int firstResult, int numberOfResultsReturned)
         {
             //given a search with some results
             var searchResults = GetJournals().Take(numberOfResults).AsQueryable();
@@ -101,6 +101,10 @@ namespace Tests.SearcherTests
             Assert.AreEqual(previousPageShouldBeAvailable, searchResponse.IsPreviousPage, "Response should know whether the page is the first page or not");
             Assert.AreEqual(nextPageShouldBeAvailable, searchResponse.IsNextPage, "Reponse should know whether the page is the last page or not.");
             Assert.AreEqual(totalResults, searchResponse.TotalResults, "Response should display the correct total number of results");
+            Assert.AreEqual(firstResult, searchResponse.FirstResult, "Response should display correct index of first result");
+            var expectedIds = Enumerable.Range(firstResult, numberOfResultsReturned).Select(x=>x.ToString());
+            var actualIds = searchResponse.Journals.Select(x=>x.Id);
+            Assert.AreEqual(expectedIds, actualIds, "Reponse should return correct results");
         }
 
 
@@ -113,7 +117,7 @@ namespace Tests.SearcherTests
         private static IEnumerable<Journal> GetJournals()
         {
             var startDate = new DateTimeOffset(new DateTime(1999, 1, 1), TimeSpan.Zero);
-            for (var i = 0; i < 3000; i++)
+            for (var i = 1; i < 3001; i++)
             {
                 yield return new Journal(i.ToString(), startDate.AddMinutes(5 * i), new DateTime(), "steve", "description", new[]
                 {
