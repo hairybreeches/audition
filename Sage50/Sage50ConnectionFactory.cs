09c81a3b284079d3b212530b1dd669fea06c15d9
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-11-11 11:04:50 +0000

    woops, the exception gets thrown elsewhere now

--- Sage50/Sage50ConnectionFactory.cs
+++ Sage50/Sage50ConnectionFactory.cs
@@ -1,5 +1,7 @@
-﻿using System.Data.Common;
+﻿using System.Data;
+using System.Data.Common;
 using System.Data.Odbc;
+using Model;
 
 namespace Sage50
 {
@@ -16,8 +18,31 @@ namespace Sage50
         {
             var connectionString = CreateConnectionString(loginDetails, driverDetector.FindBestDriver());
             var conn = new OdbcConnection(connectionString);
-            conn.Open();
+            OpenConnection(conn);                    
             return conn;
+        }
+
+        private static void OpenConnection(IDbConnection conn)
+        {
+            try
+            {
+                conn.Open();
+            }
+            catch (OdbcException e)
+            {
+                var error = e.Errors[0];
+                if (error.SQLState == "08001")
+                {
+                    throw new IncorrectLoginDetailsException(
+                        "The specified folder does not appear to be a Sage 50 data directory. The data directory can be found by logging in to Sage and clicking help->about from the menu.");
+                }
+                if (error.SQLState == "28000")
+                {
+                    throw new IncorrectLoginDetailsException("Incorrect username or password");
+                }
+
+                throw;
+            }
         }
 
         private static string CreateConnectionString(Sage50LoginDetails loginDetails, Sage50Driver driver)
--- Sage50/Sage50JournalGetter.cs
+++ Sage50/Sage50JournalGetter.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Common;
-using System.Data.Odbc;
 using System.Linq;
-using Model;
 using Model.Accounting;
-using Persistence;
 using Sage50.Parsing;
 using Sage50.Parsing.Schema;
 
@@ -26,33 +23,10 @@ namespace Sage50
 
         public IEnumerable<Journal> GetJournals(DbConnection loginDetails)
         {
-            try
-            {
-                return GetJournalsInner(loginDetails);
-            }
-            catch (OdbcException e)
-            {
-                var error = e.Errors[0];
-                if (error.SQLState == "08001")
-                {
-                    throw new IncorrectLoginDetailsException("The specified folder does not appear to be a Sage 50 data directory. The data directory can be found by logging in to Sage and clicking help->about from the menu.");
-                }
-                if (error.SQLState == "28000")
-                {
-                    throw new IncorrectLoginDetailsException("Incorrect username or password");
-                }
+                var nominalLookup = CreateNominalCodeLookup(loginDetails);
 
-                throw;
-            }
-            
-        }
-
-        private IEnumerable<Journal> GetJournalsInner(DbConnection connection)
-        {
-                var nominalLookup = CreateNominalCodeLookup(connection);
-
-                return  GetJournals(connection, nominalLookup, "AUDIT_JOURNAL")
-                    .Concat(GetJournals(connection, nominalLookup, "AUDIT_HISTORY_JOURNAL"));
+                return  GetJournals(loginDetails, nominalLookup, "AUDIT_JOURNAL")
+                    .Concat(GetJournals(loginDetails, nominalLookup, "AUDIT_HISTORY_JOURNAL"));
         }
 
         private IEnumerable<Journal> GetJournals(DbConnection connection, NominalCodeLookup nominalLookup, string tableName)
