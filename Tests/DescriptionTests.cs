065b2e0c48499cb954df88e5e551f7c8a657eb4a
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-11-14 15:57:55 +0000

    make these localised

--- Tests/DescriptionTests.cs
+++ Tests/DescriptionTests.cs
@@ -14,35 +14,35 @@ namespace Tests
         public void DescriptionCorrectForRoundNumberSearchWindow()
         {
             var window = new SearchWindow<EndingParameters>(new EndingParameters(3),new DateRange(new DateTime(1999,1,1), new DateTime(1999, 12, 31)));
-            StringAssert.AreEqualIgnoringCase("Journals Ending in at least 3 zeroes, in the period 01/01/1999 to 31/12/1999", window.Description);
+            StringAssert.AreEqualIgnoringCase(String.Format("Journals Ending in at least 3 zeroes, in the period {0} to {1}", new DateTime(1999, 1, 1).ToShortDateString(), new DateTime(1999,12,31).ToShortDateString()), window.Description);
         }
         
         [Test]
         public void DescriptionCorrectForWorkingHoursSearchWindow()
         {
             var window = new SearchWindow<WorkingHoursParameters>(new WorkingHoursParameters(DayOfWeek.Monday, DayOfWeek.Thursday, new LocalTime(9, 30),new LocalTime(18,0)), new DateRange(new DateTime(2000,4,1), new DateTime(2001, 3, 31)));
-            StringAssert.AreEqualIgnoringCase("Journals posted outside Monday to Thursday, 9:30 to 18:00, in the period 01/04/2000 to 31/03/2001", window.Description);
+            StringAssert.AreEqualIgnoringCase(String.Format("Journals posted outside Monday to Thursday, 9:30 to 18:00, in the period {0} to {1}", new DateTime(2000, 4, 1).ToShortDateString(), new DateTime(2001, 3, 31).ToShortDateString()), window.Description);
         }    
         
         [Test]
         public void DescriptionCorrectForUnusualAccountsSearchWindow()
         {
             var window = new SearchWindow<UnusualAccountsParameters>(new UnusualAccountsParameters(4), new DateRange(new DateTime(2000,4,1), new DateTime(2001, 3, 31)));
-            StringAssert.AreEqualIgnoringCase("Journals posted to nominal codes with fewer than 4 entries, in the period 01/04/2000 to 31/03/2001", window.Description);
+            StringAssert.AreEqualIgnoringCase(String.Format("Journals posted to nominal codes with fewer than 4 entries, in the period {0} to {1}", new DateTime(2000, 4, 1).ToShortDateString(), new DateTime(2001, 3, 31).ToShortDateString()), window.Description);
         }
         
         [Test]
         public void DescriptionCorrectForUserSearchWindow()
         {
             var window = new SearchWindow<UserParameters>(new UserParameters("steve\nalf\nbetty"), new DateRange(new DateTime(2000,4,1), new DateTime(2001, 3, 31)));
-            StringAssert.AreEqualIgnoringCase("Journals posted by users other than steve, alf or betty, in the period 01/04/2000 to 31/03/2001", window.Description);
+            StringAssert.AreEqualIgnoringCase(String.Format("Journals posted by users other than steve, alf or betty, in the period {0} to {1}", new DateTime(2000, 4, 1).ToShortDateString(), new DateTime(2001, 3, 31).ToShortDateString()), window.Description);
         } 
         
         [Test]
         public void DescriptionCorrectForYearEndWindow()
         {
             var window = new SearchWindow<YearEndParameters>(new YearEndParameters(5), new DateRange(new DateTime(2000,4,1), new DateTime(2001, 3, 31)));
-            StringAssert.AreEqualIgnoringCase("Journals posted after the year end or within 5 days before, in the period 01/04/2000 to 31/03/2001", window.Description);
+            StringAssert.AreEqualIgnoringCase(String.Format("Journals posted after the year end or within 5 days before, in the period {0} to {1}", new DateTime(2000, 4, 1).ToShortDateString(), new DateTime(2001, 3, 31).ToShortDateString()), window.Description);
         }
 
 
