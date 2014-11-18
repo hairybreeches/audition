4c212aa871f83bc430b07d4e4a49caadb7d6fe35
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-11-18 11:46:51 +0000

    extract this useful conversion method

---
+++ Model/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Model
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTimeOffset ToUkDateTimeOffsetFromUtc(this DateTime dateTime)
+        {
+            var dateUtc = new DateTimeOffset(dateTime, TimeSpan.Zero);
+            var timezone = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
+            var offset = timezone.GetUtcOffset(dateUtc);
+            var localDateTime = new DateTimeOffset(dateUtc.UtcDateTime, TimeSpan.Zero).ToOffset(offset);
+            return localDateTime;
+        }               
+    }
+}
