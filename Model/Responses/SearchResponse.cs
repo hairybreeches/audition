cfc43479f9caeed0db4aafc6421c2b36c4d077a4
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-11-18 11:14:27 +0000

    parse the Xero rerponse as an object to avoid worrying about DateTime formats

--- Model/Responses/SearchResponse.cs
+++ Model/Responses/SearchResponse.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Model.Accounting;
 
 namespace Model.Responses
@@ -18,6 +20,44 @@ namespace Model.Responses
         public string TotalResults { get; private set; }
         public bool IsPreviousPage { get; private set; }
         public bool IsNextPage { get; private set; }        
-        public int FirstResult { get; private set; }        
+        public int FirstResult { get; private set; }
+
+        public override string ToString()
+        {
+            return
+                String.Format(
+                    "Total results: {0}, First Result of this page: {1}, previous page: {2}, next page: {3}, journals: {4}",
+                    TotalResults, FirstResult, IsPreviousPage, IsNextPage, String.Join(", ", Journals));
+        }
+
+        protected bool Equals(SearchResponse other)
+        {
+            return Journals.SequenceEqual(other.Journals) 
+                && string.Equals(TotalResults, other.TotalResults) 
+                && IsPreviousPage.Equals(other.IsPreviousPage) 
+                && IsNextPage.Equals(other.IsNextPage) 
+                && FirstResult == other.FirstResult;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((SearchResponse) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (Journals != null ? Journals.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ (TotalResults != null ? TotalResults.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ IsPreviousPage.GetHashCode();
+                hashCode = (hashCode*397) ^ IsNextPage.GetHashCode();
+                hashCode = (hashCode*397) ^ FirstResult;
+                return hashCode;
+            }
+        }
     }
 }
