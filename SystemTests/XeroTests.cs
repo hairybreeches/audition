dd814544cb4a517abd941a668ad0065a56af62f8
hairybreeches <hairybreeches@bitbucket.org>
2014-11-18 11:24:34 +0000

    extract variable here

--- SystemTests/XeroTests.cs
+++ SystemTests/XeroTests.cs
@@ -91,12 +91,15 @@ namespace SystemTests
 
             var fileContents = File.ReadAllText(fileName);
 
-            StringAssert.AreEqualIgnoringCase(
-                @"""Journals posted outside Monday to Friday, 8:00 to 18:00, in the period 05/04/2013 to 04/04/2014""
+
+            var expected = String.Format(@"""Journals posted outside Monday to Friday, 8:00 to 18:00, in the period 05/04/2013 to 04/04/2014""
 Created,Date,Username
-" + new DateTimeOffset(2013,4,6,1,0,0,TimeSpan.FromHours(1)) +","+ new DateTime(2013,4,6).ToShortDateString() + @",,Cr,9012,Expenses,23.4,Dr,3001,Cash,23.4
-" + new DateTimeOffset(2013,4,6,1,0,0,TimeSpan.FromHours(1)) +","+ new DateTime(2013,4,6).ToShortDateString() + @",,Cr,8014,Depreciation,12.4,Dr,4001,Fixed assets,12.4
-", fileContents);
+{0},{1},,Cr,9012,Expenses,23.4,Dr,3001,Cash,23.4
+{0},{1},,Cr,8014,Depreciation,12.4,Dr,4001,Fixed assets,12.4
+", new DateTimeOffset(2013,4,6,1,0,0,TimeSpan.FromHours(1)), new DateTime(2013,4,6).ToShortDateString());
+
+
+            StringAssert.AreEqualIgnoringCase(expected, fileContents);
             
         }
 
