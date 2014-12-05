ef960abbc0c4584fdffbe6cd94a4aac60523c87b
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-12-05 10:48:22 +0000

    rename method

--- Native/IRegistryReader.cs
+++ Native/IRegistryReader.cs
@@ -4,7 +4,7 @@ namespace Native
 {
     public interface IRegistryReader
     {
-        bool TryGetKeyValue(string licenceKeyLocation, string licenceKeyName, out string licenceKey);
+        bool TryGetStringValue(string licenceKeyLocation, string licenceKeyName, out string licenceKey);
         bool TryGetValueNames(string location, out IEnumerable<string> valueNames);
     }
 }
