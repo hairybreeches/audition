d031eac3ba2c922c1ef596c41618e72c5b9d32bd
hairybreeches <hairybreeches@bitbucket.org>
2014-12-05 10:32:09 +0000

    this logic belongs in the regsitry reader

--- Native/IRegistryReader.cs
+++ Native/IRegistryReader.cs
@@ -3,5 +3,6 @@
     public interface IRegistryReader
     {
         bool TryOpenKey(string keyName, out IRegistryKey registryKey);
+        bool TryGetKeyValue(string licenceKeyLocation, string licenceKeyName, out string licenceKey);
     }
 }
