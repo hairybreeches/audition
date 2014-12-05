1af572fcd7ad4995b12bc622439facfc0cfb9015
hairybreeches <hairybreeches@bitbucket.org>
2014-12-05 16:42:55 +0000

    add a checksum for licence keys

--- Licensing/LicenceVerifier.cs
+++ Licensing/LicenceVerifier.cs
@@ -1,4 +1,9 @@
-﻿namespace Licensing
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace Licensing
 {
     public class LicenceVerifier
     {
@@ -8,6 +13,31 @@
             {
                 throw new InvalidLicenceKeyException();
             }
+          
+            var firstTenDigits = licenceKey.Take(10);
+
+            var checkSum = GetExpectedCheckSum(firstTenDigits);
+
+            var lastSixDigits = licenceKey.Substring(10);
+            if (!Equals(checkSum, lastSixDigits))
+            {
+                throw new InvalidLicenceKeyException();
+            }
+
+        }
+
+        private static string GetExpectedCheckSum(IEnumerable<char> hashInput)
+        {
+            using (SHA512 shaM = new SHA512Managed())
+            {
+                var hash = shaM.ComputeHash(GetBytes(hashInput));
+                return BitConverter.ToString(hash.Take(3).ToArray()).Replace("-", "");
+            }
+        }
+
+        static byte[] GetBytes(IEnumerable<char> characters)
+        {
+            return characters.Select(x => (byte) x).ToArray();            
         }
     }
 }
