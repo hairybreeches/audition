f1fe73cdc8609803c981e10e5285d9def7e9a41d
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-12-05 15:14:13 +0000

    add the write access to the interface

--- Native/IRegistry.cs
+++ Native/IRegistry.cs
@@ -8,6 +8,7 @@ namespace Native
         bool TryGetValueNames(string location, out IEnumerable<string> valueNames);
     }
 
+    //although we will have write access to the Current User registry hive, since we won't be admin it's unlikely we will do Local Machine
     public interface ILocalMachineRegistry : IRegistry
     {
         
@@ -15,6 +16,7 @@ namespace Native
 
     public interface ICurrentUserRegistry : IRegistry
     {
-        
+        void CreateLocation(string location);
+        void WriteValue(string location, string name, string value);
     }
 }
