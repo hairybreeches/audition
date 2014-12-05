8da86c944c979a2a921f5572a033d6d1e9f14fb3
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-12-05 10:00:16 +0000

    licensing classes and first test

---
+++ Licensing/LicenceReader.cs
@@ -0,0 +1,20 @@
+﻿using Native;
+
+namespace Licensing
+{
+    public class LicenceReader
+    {
+        private readonly IRegistryReader registryReader;
+        private const string LicenceKeyLocation = "SOFTWARE\\Audition\\Audition\\Licence key";
+
+        public LicenceReader(IRegistryReader registryReader)
+        {
+            this.registryReader = registryReader;
+        }
+
+        public ILicence GetLicence()
+        {
+            return new Licence(false);
+        }
+    }
+}
---
+++ Tests/LicensingTests.cs
@@ -0,0 +1,21 @@
+﻿using Licensing;
+using NUnit.Framework;
+using Tests.Mocks;
+
+namespace Tests
+{
+    [TestFixture]
+    public class LicensingTests
+    {
+        [Test]
+        public void WhenNoLicenceKeyExistsProductIsUnlicensed()
+        {
+            //given a registry with no licence key stored in it
+            var licenceReader = new LicenceReader(new MockRegistryReader());
+            //when we retrieve the licence
+            var licence = licenceReader.GetLicence();
+            //then the licence will say it's not been fully licensed yet
+            Assert.AreEqual(false, licence.IsFullyLicensed, "When there is no licence key in the registry, the product should not be fully licensed");
+        }
+    }
+}
