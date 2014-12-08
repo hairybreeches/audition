a7c584225dee63b96e48b9afe64ac901f8c7adee
hairybreeches <hairybreeches@bitbucket.org>
2014-12-08 10:13:44 +0000

    a controller for the functionality we've added

---
+++ Webapp/Controllers/LicensingController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Http;
+using Licensing;
+
+namespace Webapp.Controllers
+{
+    public class LicensingController : ApiController
+    {
+        private readonly LicenceStorage storage;
+
+        public LicensingController(LicenceStorage storage)
+        {
+            this.storage = storage;
+        }
+
+
+        [HttpGet]
+        [Route(Routing.GetLicence)]
+        public ILicence GetLicence()
+        {
+            return storage.GetLicence();
+        }
+
+        [HttpPost]
+        [Route(Routing.UpdateLicence)]
+        public ILicence UpdateLicence(string licenceKey)
+        {
+            storage.StoreLicence(licenceKey);
+            return GetLicence();
+        }
+    }
+}
