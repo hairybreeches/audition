aa28eafce05ec2b9b1c33cb3fbd964fa0e807b8f
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2014-12-09 11:48:42 +0000

    display licence status

--- Ui/Ui/scripts/licensing.js
+++ Ui/Ui/scripts/licensing.js
@@ -1,19 +1,27 @@
 var licenceModel = ko.mapping.fromJS({
-    IsFullyLicensed: true
+    IsFullyLicensed: true,
+    TrialValid: true,
+    RemainingTrialDays: 28    
 });
 
-licenceModel.showUnlicensed = ko.computed(function() {
-    return ! licenceModel.IsFullyLicensed();
-})
+licenceModel.licenceText = ko.computed(function () {
+
+    if (licenceModel.IsFullyLicensed()) {
+        return "";
+    }
+
+    if (licenceModel.TrialValid()) {
+        return licenceModel.RemainingTrialDays() + " days of trial remaining";
+    }
+
+    return "Unlicensed";
+});
 
 ko.applyBindings(licenceModel, document.getElementById('licensingElement'));
 
 var updateLicence = function (newLicence) {
     ko.mapping.fromJS(newLicence, licenceModel);
 }
-
-
-
 
 $.ajax("/api/licence/get", {
     success: updateLicence,
