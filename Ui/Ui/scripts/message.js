20796919b8032c11705f77aa01a216e0ec2c0cd1
hairybreeches <hairybreeches@bitbucket.org>
2014-12-10 13:34:55 +0000

    and the javascript too

--- Ui/Ui/scripts/message.js
+++ Ui/Ui/scripts/message.js
@@ -2,16 +2,31 @@ var Message = function () {
     var self = this;
     //fields
     self.visible = ko.observable(false);
-    self.message = ko.observable('');
 
     //methods
     self.hide = function () {
         self.visible(false);
     };
 
-    self.show = function (jqXHR) {
-        var errorMessage = getErrorMessage(jqXHR);
-        self.message(errorMessage);
+    self.show = function () {
         self.visible(true);
     }
 };
+
+var ErrorMessage = function() {
+    var self = this;
+    var message = new Message();
+
+    //fields
+    self.visible = message.visible;
+    self.message = ko.observable('');
+
+    //methods
+    self.hide = message.hide;
+
+    self.show = function (jqXHR) {
+        var errorMessage = getErrorMessage(jqXHR);
+        self.message(errorMessage);
+        message.visible(true);
+    }
+}
