6b04a2d3eb1c7b707f1bdfa8eeda669dcfbd3171
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-01-07 11:48:24 +0000

    add a way to retrieve these locations in the webapp

---
+++ Webapp/Controllers/UserDataController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using UserData;
+
+namespace Webapp.Controllers
+{
+    public class UserDataController : ApiController
+    {
+        private readonly UserDetails userDetails;
+
+        public UserDataController(UserDetails userDetails)
+        {
+            this.userDetails = userDetails;
+        }
+
+        [HttpGet]
+        [Route(Routing.Sage50DataLocations)]        
+        public IEnumerable<string> SageDataLocations()
+        {
+            return userDetails.Sage50DataLocations;
+        }
+    }
+}
