c0cd34de0dc0b10556757180ef0cc6e5e926540d
hairybreeches <hairybreeches@bitbucket.org>
2015-01-07 11:43:49 +0000

    integrate user details into the app and store the sage location in it

---
+++ UserData/UserDetails.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace UserData
+{
+    public class UserDetails
+    {
+        private readonly MostRecentList sageDataLocations = new MostRecentList();        
+
+
+        public IEnumerable<string> Sage50DataLocations
+        {
+            get { return sageDataLocations.GetMostRecentValues(); }
+        }
+
+        public void AddSage50DataLocation(string location)
+        {
+            sageDataLocations.AddUsage(location);
+        }
+    }
+}
