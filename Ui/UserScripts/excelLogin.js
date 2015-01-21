b3acbb1282e617b4defe396d0259208e21cf50dc
hairybreeches <hairybreeches@bitbucket.org>
2015-01-21 16:22:40 +0000

    give sage 50 and excel login sections their own file

---
+++ Ui/UserScripts/excelLogin.js
@@ -0,0 +1,29 @@
+﻿var ExcelLoginModel = function() {
+    var self = this;
+
+    self.fileLocation = ko.observable('');
+    var fields = ['JournalDate', 'Description', 'Username', 'Created', 'AccountCode', 'AccountName', 'Amount', 'JournalType'];
+    fields.forEach(
+        function(fieldName) {
+            self[fieldName] = ko.observable('A');
+        });
+
+    var getData = function() {
+        var data = {
+            Filename: self.fileLocation()
+        };
+
+        data.Lookups = fields.reduce(function(lookups, fieldName) {
+            lookups[fieldName] = self[fieldName]();
+            return lookups;
+        }, {});
+
+        return data;
+    }
+
+    self.browseExcelFile = createBrowseFunction('/api/chooseExcelFile', self.fileLocation);
+
+    self.submit = function() {
+        model.login('/api/excel/login', getData());
+    };
+};
