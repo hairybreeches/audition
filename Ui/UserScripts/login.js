1d26343b527833ea9932de389c62a61db223b9f2
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-01-21 11:32:03 +0000

    first, very rough stab at UI for excel login

--- Ui/UserScripts/login.js
+++ Ui/UserScripts/login.js
@@ -27,11 +27,40 @@
     }    
 }
 
+var ExcelLoginModel = function() {
+    var self = this;
+
+    self.fileLocation = ko.observable('');
+    var fields = ['JournalDate', 'Description', 'Username', 'Created', 'AccountCode', 'AccountName', 'Amount', 'JournalType'];
+    fields.forEach(
+        function(fieldName) {
+            self[fieldName] = ko.observable('A');
+        });
+
+    var getData = function() {
+        var data = {
+            Filename: self.fileLocation()
+        };
+
+        data.Lookups = fields.reduce(function(lookups, fieldName) {
+            lookups[fieldName] = self[fieldName]();
+            return lookups;
+        }, {});
+
+        return data;
+    }
+
+    self.submit = function() {
+        model.login('/api/excel/login', getData());
+    };
+}
+
 var LoginModel = function() {
     var self = this;
     self.blocked = ko.observable(false);
     self.system = ko.observable('');
     self.sage50 = new Sage50LoginModel();
+    self.excel = new ExcelLoginModel();
     self.error = new ErrorMessage();
 
     var goToSearch = function() {
@@ -51,9 +80,10 @@ var LoginModel = function() {
     self.login = function(url, data) {
         startLogin();
         $.ajax({
+            contentType: 'application/json',
             type: "POST",
             url: url,
-            data: data,
+            data: JSON.stringify(data),
             success: goToSearch,
             error: showError
         });
