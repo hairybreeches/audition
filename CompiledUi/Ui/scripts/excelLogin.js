5219129689b542b74785f8eabbc5ba6a62daf531
hairybreeches <hairybreeches@bitbucket.org>
2015-01-23 10:14:19 +0000

    Update column dropdowns depending on spreadsheet selected

--- CompiledUi/Ui/scripts/excelLogin.js
+++ CompiledUi/Ui/scripts/excelLogin.js
@@ -21,6 +21,21 @@ var ExcelLoginModel = function() {
         return data;
     }
 
+    self.columnNames = ko.observableArray(['Enter an excel spreadsheet name above']);    
+
+    var updateColumnNames = function(fileLocation) {
+        $.ajax('/api/excel/getHeaders', {
+            data: {
+                filename: fileLocation
+            },
+            success: function(data) {
+                self.columnNames(data);
+            }
+        });
+    }
+
+    self.fileLocation.subscribe(updateColumnNames);
+
     self.browseExcelFile = createBrowseFunction('/api/chooseExcelFile', self.fileLocation);
 
     self.submit = function() {
