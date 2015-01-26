d18f85d26c7f2f6d45a0cc966be50c930578838e
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-01-26 11:54:05 +0000

    move mapping data files back to excel project

--- ExcelImport/FieldLookups.cs
+++ ExcelImport/FieldLookups.cs
@@ -1,4 +1,4 @@
-namespace Model
+namespace ExcelImport
 {
     public class FieldLookups
     {
--- ExcelImport/HeaderRowData.cs
+++ ExcelImport/HeaderRowData.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Model
+namespace ExcelImport
 {
     public class HeaderRowData
     {
