bdbde255cbd71162d011fc9e2f9f8dce01390049
hairybreeches <hairybreeches@bitbucket.org>
2015-01-29 16:45:04 +0000

    add test

---
+++ SystemTests/ExcelImportTests.cs
@@ -0,0 +1,44 @@
+﻿using Autofac;
+using ExcelImport;
+using NUnit.Framework;
+using Tests;
+using Webapp.Controllers;
+
+namespace SystemTests
+{
+    [TestFixture]
+    public class ExcelImportTests
+    {
+        [Test]
+        public void CanImportExcelData()
+        {
+            var builder = AutofacConfiguration.CreateDefaultContainerBuilder();
+            using (var scope = builder.Build())
+            {
+                var controller = scope.Resolve<ExcelSessionController>();
+                controller.ExcelLogin(new ExcelImportMapping
+                {
+                    SheetData = new HeaderRowData
+                    {
+                        Filename = "..\\..\\..\\ExcelImport\\ExampleSage50Export.xlsx",
+                        Sheet = 0,
+                        UseHeaderRow = true,
+                        SkipRows = 2
+                    },
+
+                    Lookups = new FieldLookups
+                    {
+                        AccountCode = 2,
+                        AccountName = -1,
+                        Amount = 9,
+                        Created = -1,
+                        Description = 5,
+                        JournalDate = 6,
+                        Username = 19
+                    }
+
+                });
+            }
+        }
+    }
+}
