f989b7c8256791fad28f8878b582e4ed2e2dcfe8
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-01-29 15:16:48 +0000

    move type

--- Sage50/Parsing/Schema/JournalSchema.cs
+++ Sage50/Parsing/Schema/JournalSchema.cs
@@ -94,27 +94,4 @@ namespace Sage50.Parsing.Schema
             return nominalCodeNameColumn.GetField(record);
         }
     }
-
-    public class UnmappedColumn<T> : ISchemaColumn<T>
-    {
-        public T GetField(IDataRecord record)
-        {
-            return default(T);
-        }
-
-        public DataColumn ToDataColumn()
-        {
-            return null;
-        }
-
-        public int Index
-        {
-            get { return -1; }
-        }
-
-        public string FieldName
-        {
-            get { return String.Empty; }
-        }
-    }
 }
