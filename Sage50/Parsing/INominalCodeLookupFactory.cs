8ac4e25e9adc26ba49a62771159d915540de771c
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-01-30 12:55:36 +0000

    make code private

--- Sage50/Parsing/INominalCodeLookupFactory.cs
+++ Sage50/Parsing/INominalCodeLookupFactory.cs
@@ -6,6 +6,5 @@ namespace Sage50.Parsing
     public interface INominalCodeLookupFactory
     {
         NominalCodeLookup FromQueryResult(IDataReader reader);
-        void AddNominalCode(Dictionary<string, string> dictionary, string nominalCode, string nominalCodeName);
     }
 }
