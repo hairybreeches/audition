9f491d2940c2fe75bdbd8285f775aac12fb3b221
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-01-30 11:37:46 +0000

    YAGNI

--- ExcelImport/SheetMetadata.cs
+++ ExcelImport/SheetMetadata.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace ExcelImport
 {
     public class SheetMetadata
@@ -7,29 +5,5 @@ namespace ExcelImport
         public string Filename { get; set; }
         public int Sheet { get; set; }
         public bool UseHeaderRow { get; set; }
-
-        protected bool Equals(SheetMetadata other)
-        {
-            return string.Equals(Filename, other.Filename, StringComparison.InvariantCultureIgnoreCase) && Sheet == other.Sheet && UseHeaderRow.Equals(other.UseHeaderRow);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((SheetMetadata) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hashCode = (Filename != null ? Filename.ToLowerInvariant().GetHashCode() : 0);
-                hashCode = (hashCode*397) ^ Sheet;
-                hashCode = (hashCode*397) ^ UseHeaderRow.GetHashCode();
-                return hashCode;
-            }
-        }
     }
 }
