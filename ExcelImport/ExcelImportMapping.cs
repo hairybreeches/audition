55d7da83b1e5b58c4b256b5ece13a743ef9f8f2f
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-01-30 12:40:59 +0000

    YAGNI

--- ExcelImport/ExcelImportMapping.cs
+++ ExcelImport/ExcelImportMapping.cs
@@ -5,27 +5,6 @@ namespace ExcelImport
     public class ExcelImportMapping
     {
         public SheetMetadata SheetData { get; set; }
-        public FieldLookups Lookups { get; set; }
-
-        protected bool Equals(ExcelImportMapping other)
-        {
-            return Equals(SheetData, other.SheetData) && Equals(Lookups, other.Lookups);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((ExcelImportMapping) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return ((SheetData != null ? SheetData.GetHashCode() : 0)*397) ^ (Lookups != null ? Lookups.GetHashCode() : 0);
-            }
-        }
+        public FieldLookups Lookups { get; set; }       
     }
 }
