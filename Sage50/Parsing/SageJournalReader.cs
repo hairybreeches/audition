2b2b0d011b27f99832cd159ccfc04e7e2bfa71e6
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-01-30 16:21:04 +0000

    do the nominal code lookup using readers

--- Sage50/Parsing/SageJournalReader.cs
+++ Sage50/Parsing/SageJournalReader.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Data;
-using System.Data.Common;
-using System.Linq;
 using Model;
 using Model.Accounting;
 using SqlImport;
@@ -11,8 +9,8 @@ namespace Sage50.Parsing
     public class SageJournalReader
     {
         
-        private readonly JournalSchema schema;
         private readonly SqlJournalReader sqlJournalReader;
+        private readonly SageJournalSchema schema;
 
         public SageJournalReader(SageJournalSchema schema, SqlJournalReader sqlJournalReader)
         {
@@ -20,20 +18,11 @@ namespace Sage50.Parsing
             this.sqlJournalReader = sqlJournalReader;
         }
 
-
-        private SqlJournalLine AddNominalCode(SqlJournalLine journalLine, NominalCodeLookup lookup)
-        {
-            //a little cheeky, that we mutate rather than creating a new object here, but there could be a lot of these
-            //and the SqlJournalLine is an "in progress" object
-            journalLine.NominalCodeName = lookup.GetLookupValue(journalLine.NominalCode);
-            return journalLine;
-        }
-
         public IEnumerable<Journal> GetJournals(IDataReader dataReader, NominalCodeLookup nominalLookup)
         {
             if (dataReader.Read())
             {
-                return sqlJournalReader.GetJournals(dataReader, schema, line => AddNominalCode(line, nominalLookup));
+                return sqlJournalReader.GetJournals(dataReader, schema.CreateJournalSchema(nominalLookup));
             }
             else
             {
