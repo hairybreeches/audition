7b4212c1f1e16c04ddd492ef3aa6b6a213bebb06
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-02-02 15:30:34 +0000

    separate out the data class and the work class

--- ExcelImport/ExcelSearcherFactoryFactory.cs
+++ ExcelImport/ExcelSearcherFactoryFactory.cs
@@ -7,12 +7,12 @@ namespace ExcelImport
 {
     public class ExcelSearcherFactoryFactory
     {
-        public IJournalSearcherFactory CreateSearcherFactory(FieldLookups lookups)
+        public IJournalSearcherFactory CreateSearcherFactory(ExcelDataMapper excelDataMapper)
         {
-            return new JournalSearcherFactory(UnvailableActionMessages(lookups), lookups.GetDisplayableFields());
+            return new JournalSearcherFactory(UnvailableActionMessages(excelDataMapper), excelDataMapper.GetDisplayableFields());
         }
 
-        private static IDictionary<SearchAction, string> UnvailableActionMessages(FieldLookups lookups)
+        private static IDictionary<SearchAction, string> UnvailableActionMessages(ExcelDataMapper lookups)
         {
             return lookups.GetUnavailableActions()
                 .Aggregate(new Dictionary<SearchAction, string>(),
