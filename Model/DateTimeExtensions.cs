60415ef2b31ed36b6ba183630bc09a7a6450fdd5
hairybreeches <hairybreeches@bitbucket.org>
2015-02-02 20:20:42 +0000

    delete unused

--- Model/DateTimeExtensions.cs
+++ Model/DateTimeExtensions.cs
@@ -4,24 +4,11 @@ namespace Model
 {
     public static class DateTimeExtensions
     {
-        public static DateTimeOffset ToUkDateTimeOffsetFromUtc(this DateTime dateTime)
-        {
-            var dateUtc = new DateTimeOffset(dateTime, TimeSpan.Zero);
-            var offset = GetUkOffset(dateUtc);
-            return dateUtc.ToOffset(offset);
-        }
-        
         public static DateTimeOffset ToUkDateTimeOffsetFromUkLocalTime(this DateTime dateTime)
         {
             return new DateTimeOffset(dateTime, GetUkOffset(dateTime));
         }
 
-        private static TimeSpan GetUkOffset(DateTimeOffset dateTimeOffset)
-        {
-            var timezone = GetUkTimezone();
-            return timezone.GetUtcOffset(dateTimeOffset);
-        }
-        
         private static TimeSpan GetUkOffset(DateTime ukDateTime)
         {
             var timezone = GetUkTimezone();
