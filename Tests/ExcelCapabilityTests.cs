1ddd003f05c1e36eea245f4f9d3d31e848f25749
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-02-04 15:05:45 +0000

    create method for making test cases

--- Tests/ExcelCapabilityTests.cs
+++ Tests/ExcelCapabilityTests.cs
@@ -9,77 +9,6 @@ namespace Tests
     [TestFixture]
     public class ExcelCapabilityTests
     {
-        public IEnumerable<TestCaseData> SingleFieldMapped
-        {
-            get
-            {
-                yield return new TestCaseData(new FieldLookups(id: 1, accountCode: -1, accountName: -1, amount: -1, created: -1, description: -1, journalDate: 18, username: -1))
-                    .Returns(new SearchCapability(new[] { DisplayField.JournalDate }, new Dictionary<string, string>().WithAllErrorMessages()))
-                    .SetName("ID mapped");
-
-                yield return new TestCaseData(new FieldLookups(id: -1, accountCode: 12, accountName: -1, amount: -1, created: -1, description: -1, journalDate: 18, username: -1))
-                    .Returns(new SearchCapability(new[] { DisplayField.JournalDate, DisplayField.AccountCode }, new Dictionary<string, string>().WithAllErrorMessages().Without(SearchAction.Accounts)))
-                    .SetName("Account Code mapped");
-
-                yield return new TestCaseData(new FieldLookups(id: -1, accountCode: -1, accountName: 3, amount: -1, created: -1, description: -1, journalDate: 18, username: -1))
-                        .Returns(new SearchCapability(new[] { DisplayField.JournalDate, DisplayField.AccountName },new Dictionary<string, string>().WithAllErrorMessages()))
-                        .SetName("Account name mapped");
-
-                yield return new TestCaseData(new FieldLookups(id: -1, accountCode: -1, accountName: -1, amount: 5, created: -1, description: -1, journalDate: 18, username: -1))
-                        .Returns(new SearchCapability(new[] { DisplayField.JournalDate, DisplayField.JournalType, DisplayField.Amount }, new Dictionary<string, string>().WithAllErrorMessages().Without(SearchAction.Ending)))
-                        .SetName("Amount mapped");
-
-                yield return new TestCaseData(new FieldLookups(id: -1, accountCode: -1, accountName: -1, amount: -1, created: 7, description: -1, journalDate: 18, username: -1))
-                        .Returns(new SearchCapability(new[] { DisplayField.Created, DisplayField.JournalDate }, new Dictionary<string, string>().WithAllErrorMessages().Without(SearchAction.Date).Without(SearchAction.Hours)))
-                        .SetName("Creation date mapped");
-
-                yield return new TestCaseData(new FieldLookups(id: -1, accountCode: -1, accountName: -1, amount: -1, created: -1, description: 4, journalDate: 18, username: -1))
-                        .Returns(new SearchCapability(new[] { DisplayField.JournalDate, DisplayField.Description }, new Dictionary<string, string>().WithAllErrorMessages()))
-                        .SetName("Description mapped");
-
-                yield return new TestCaseData(new FieldLookups(id: -1, accountCode: -1, accountName: -1, amount: -1, created: -1, description: -1, journalDate: 18, username: 319))
-                        .Returns(new SearchCapability(new[] { DisplayField.JournalDate, DisplayField.Username }, new Dictionary<string, string>().WithAllErrorMessages().Without(SearchAction.Users)))
-                        .SetName("Username mapped");
-            }
-        }               
-
-        public IEnumerable<TestCaseData> SingleFieldUnmapped
-        {
-            get
-            {
-                yield return new TestCaseData(new FieldLookups(id: -1, accountCode: 18, accountName: 18, amount: 18, created: 18, description: 18, journalDate: 18, username: 18))
-                    .Returns(new SearchCapability(Enums.GetAllValues<DisplayField>(), new Dictionary<string, string>()))
-                    .SetName("ID unmapped");
-
-                yield return new TestCaseData(new FieldLookups(id: 18, accountCode: -1, accountName: 18, amount: 18, created: 18, description: 18, journalDate: 18, username: 18))
-                    .Returns(new SearchCapability(Enums.GetAllValues<DisplayField>().Without(DisplayField.AccountCode), new Dictionary<string, string>().WithAccountsErrorMessage()))
-                    .SetName("Account Code unmapped");
-
-                yield return new TestCaseData(new FieldLookups(id: 18, accountCode: 18, accountName: -1, amount: 18, created: 18, description: 18, journalDate: 18, username: 18))
-                        .Returns(new SearchCapability(Enums.GetAllValues<DisplayField>().Without(DisplayField.AccountName), new Dictionary<string, string>()))
-                        .SetName("Account name unmapped");
-
-                yield return new TestCaseData(new FieldLookups(id: 18, accountCode: 18, accountName: 18, amount: -1, created: 18, description: 18, journalDate: 18, username: 18))
-                        .Returns(new SearchCapability(Enums.GetAllValues<DisplayField>().Without(DisplayField.Amount).Without(DisplayField.JournalType), new Dictionary<string, string>().WithEndingErrorMessage()))
-                        .SetName("Amount unmapped");
-
-                yield return new TestCaseData(new FieldLookups(id: 18, accountCode: 18, accountName: 18, amount: 18, created: -1, description: 18, journalDate: 18, username: 18))
-                        .Returns(new SearchCapability(Enums.GetAllValues<DisplayField>().Without(DisplayField.Created), new Dictionary<string, string>()
-                        .WithHoursErrorMessage()
-                        .WithYearEndErrorMessage()))
-                        .SetName("Journal creation time unmapped");
-
-                yield return new TestCaseData(new FieldLookups(id: 18, accountCode: 18, accountName: 18, amount: 18, created: 18, description: -1, journalDate: 18, username: 18))
-                        .Returns(new SearchCapability(Enums.GetAllValues<DisplayField>().Without(DisplayField.Description), new Dictionary<string, string>()))
-                        .SetName("Description unmapped");
-
-                yield return new TestCaseData(new FieldLookups(id: 18, accountCode: 18, accountName: 18, amount: 18, created: 18, description: 18, journalDate: 18, username: -1))
-                        .Returns(new SearchCapability(Enums.GetAllValues<DisplayField>().Without(DisplayField.Username), new Dictionary<string, string>().WithUsersErrorMessage()))
-                        .SetName("Username unmapped");
-            }
-
-
-        }
         [TestCaseSource("SingleFieldMapped")]
         [TestCaseSource("SingleFieldUnmapped")]
         public SearchCapability GetSearchCapability(FieldLookups lookups)
@@ -87,6 +16,88 @@ namespace Tests
             var factoryFactory = new ExcelSearcherFactoryFactory(new ExcelDataMapper(new ExcelColumnNamer()));
             var searcherFactory = factoryFactory.CreateSearcherFactory(lookups);
             return searcherFactory.GetSearchCapability();
+        }
+
+        public IEnumerable<TestCaseData> SingleFieldMapped
+        {
+            get
+            {
+                yield return CreateTestCase(new FieldLookups(id: 1, accountCode: -1, accountName: -1, amount: -1, created: -1, description: -1, journalDate: 18, username: -1), new[] { DisplayField.JournalDate }, new Dictionary<string, string>().WithAllErrorMessages(), "ID mapped");
+
+                yield return CreateTestCase(new FieldLookups(id: -1, accountCode: 12, accountName: -1, amount: -1, created: -1, description: -1, journalDate: 18, username: -1),
+                    new[] { DisplayField.JournalDate, DisplayField.AccountCode }, 
+                    new Dictionary<string, string>().WithAllErrorMessages().Without(SearchAction.Accounts),
+                    "Account Code mapped");
+
+                yield return CreateTestCase(new FieldLookups(id: -1, accountCode: -1, accountName: 3, amount: -1, created: -1, description: -1, journalDate: 18, username: -1),
+                        new[] { DisplayField.JournalDate, DisplayField.AccountName },
+                        new Dictionary<string, string>().WithAllErrorMessages(),
+                        "Account name mapped");
+
+                yield return CreateTestCase(new FieldLookups(id: -1, accountCode: -1, accountName: -1, amount: 5, created: -1, description: -1, journalDate: 18, username: -1),
+                        new[] { DisplayField.JournalDate, DisplayField.JournalType, DisplayField.Amount }, 
+                        new Dictionary<string, string>().WithAllErrorMessages().Without(SearchAction.Ending),
+                        "Amount mapped");
+
+                yield return CreateTestCase(new FieldLookups(id: -1, accountCode: -1, accountName: -1, amount: -1, created: 7, description: -1, journalDate: 18, username: -1),
+                        new[] { DisplayField.Created, DisplayField.JournalDate }, 
+                        new Dictionary<string, string>().WithAllErrorMessages().Without(SearchAction.Date).Without(SearchAction.Hours),
+                        "Creation date mapped");
+
+                yield return CreateTestCase(new FieldLookups(id: -1, accountCode: -1, accountName: -1, amount: -1, created: -1, description: 4, journalDate: 18, username: -1),
+                        new[] { DisplayField.JournalDate, DisplayField.Description }, 
+                        new Dictionary<string, string>().WithAllErrorMessages(),
+                        "Description mapped");
+
+                yield return CreateTestCase(new FieldLookups(id: -1, accountCode: -1, accountName: -1, amount: -1, created: -1, description: -1, journalDate: 18, username: 319),
+                        new[] { DisplayField.JournalDate, DisplayField.Username }, 
+                        new Dictionary<string, string>().WithAllErrorMessages().Without(SearchAction.Users),
+                        "Username mapped");
+            }
+        }
+
+        public IEnumerable<TestCaseData> SingleFieldUnmapped
+        {
+            get
+            {
+                yield return CreateTestCase(new FieldLookups(id: -1, accountCode: 18, accountName: 18, amount: 18, created: 18, description: 18, journalDate: 18, username: 18),
+                    Enums.GetAllValues<DisplayField>(), new Dictionary<string, string>(),
+                    "ID unmapped");
+
+                yield return CreateTestCase(new FieldLookups(id: 18, accountCode: -1, accountName: 18, amount: 18, created: 18, description: 18, journalDate: 18, username: 18),
+                    Enums.GetAllValues<DisplayField>().Without(DisplayField.AccountCode), new Dictionary<string, string>().WithAccountsErrorMessage(),
+                    "Account Code unmapped");
+
+                yield return CreateTestCase(new FieldLookups(id: 18, accountCode: 18, accountName: -1, amount: 18, created: 18, description: 18, journalDate: 18, username: 18),
+                        Enums.GetAllValues<DisplayField>().Without(DisplayField.AccountName), new Dictionary<string, string>(),
+                        "Account name unmapped");
+
+                yield return CreateTestCase(new FieldLookups(id: 18, accountCode: 18, accountName: 18, amount: -1, created: 18, description: 18, journalDate: 18, username: 18),
+                        Enums.GetAllValues<DisplayField>().Without(DisplayField.Amount).Without(DisplayField.JournalType), new Dictionary<string, string>().WithEndingErrorMessage(),
+                        "Amount unmapped");
+
+                yield return CreateTestCase(new FieldLookups(id: 18, accountCode: 18, accountName: 18, amount: 18, created: -1, description: 18, journalDate: 18, username: 18),
+                        Enums.GetAllValues<DisplayField>().Without(DisplayField.Created), new Dictionary<string, string>()
+                        .WithHoursErrorMessage()
+                        .WithYearEndErrorMessage(),
+                        "Journal creation time unmapped");
+
+                yield return CreateTestCase(new FieldLookups(id: 18, accountCode: 18, accountName: 18, amount: 18, created: 18, description: -1, journalDate: 18, username: 18),
+                        Enums.GetAllValues<DisplayField>().Without(DisplayField.Description), new Dictionary<string, string>(),
+                        "Description unmapped");
+
+                yield return CreateTestCase(new FieldLookups(id: 18, accountCode: 18, accountName: 18, amount: 18, created: 18, description: 18, journalDate: 18, username: -1),
+                        Enums.GetAllValues<DisplayField>().Without(DisplayField.Username), new Dictionary<string, string>().WithUsersErrorMessage(),
+                        "Username unmapped");
+            }
+        }
+
+
+        private static TestCaseData CreateTestCase(FieldLookups fieldLookups, IList<DisplayField> availableFields, IDictionary<string, string> errorMessages, string name)
+        {
+            return new TestCaseData(fieldLookups)
+                .Returns(new SearchCapability(availableFields, errorMessages))
+                .SetName(name);
         }
     }
 }
