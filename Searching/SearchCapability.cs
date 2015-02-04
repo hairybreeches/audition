fbfcdc8196d52555ce65c2db1c7d0492d3aa4d14
hairybreeches <hairybreeches@bitbucket.org>
2015-02-04 14:48:36 +0000

    extend the tests to include the SearchCapability

--- Searching/SearchCapability.cs
+++ Searching/SearchCapability.cs
@@ -13,15 +13,38 @@ namespace Searching
         public IList<DisplayField> AvailableFields { get; private set; }
         public IDictionary<string, string> UnvailableActionMessages { get; private set; }        
         
-        public SearchCapability(IList<DisplayField> availableFields, IDictionary<SearchAction, string> unvailableActionMessages)
+        public SearchCapability(IList<DisplayField> availableFields, IDictionary<string, string> unvailableActionMessages)
         {
             AvailableFields = availableFields;
-            UnvailableActionMessages = unvailableActionMessages.Aggregate(new Dictionary<string, string>(),
-                (dictionary, kvp) =>
-                {
-                    dictionary.Add(kvp.Key.ToString(), kvp.Value);
-                    return dictionary;
-                });
-        }        
+            UnvailableActionMessages = unvailableActionMessages;
+        }
+
+        protected bool Equals(SearchCapability other)
+        {
+            return AvailableFields.SequenceEqual(other.AvailableFields) && UnvailableActionMessages.SequenceEqual(other.UnvailableActionMessages);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((SearchCapability) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((AvailableFields != null ? AvailableFields.GetHashCode() : 0)*397) ^ (UnvailableActionMessages != null ? UnvailableActionMessages.GetHashCode() : 0);
+            }
+        }
+
+        public override string ToString()
+        {
+            var message = "Available fields: " + String.Join(", ", AvailableFields);
+            return UnvailableActionMessages.Aggregate(message, 
+                (currentMessage, pair) => currentMessage + "\r\n" + pair.Key + " unavailable: " + pair.Value);
+        }
     }
 }
