dc7f5ea2d376d01db2d2369db9ccbc3c52124428
hairybreeches <hairybreeches@bitbucket.org>
2015-02-05 14:34:09 +0000

    rename tests

--- Tests/MetadataReaderTests.cs
+++ Tests/MetadataReaderTests.cs
@@ -6,7 +6,7 @@ using NUnit.Framework;
 namespace Tests
 {
     [TestFixture]
-    public class HeaderReadingTests
+    public class MetadataReaderTests
     {
         [Test]
         public void WithoutHeaderRowHeaderReaderReturnsLetteredListOfCorrectSize()
