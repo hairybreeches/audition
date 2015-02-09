57f28a892a7abd9767799aa5f2748a5440a6046c
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-02-09 17:50:09 +0000

    move method

--- Tests/Searching.cs
+++ Tests/Searching.cs
@@ -26,19 +26,6 @@ namespace Tests
 
             var searcher = JournalSearcherFactory.EverythingAvailable.CreateJournalSearcher(repo);
             return searchWindow.Execute(searcher);
-        }
-
-        public static IEnumerable<Journal> ExecuteSearch(ISearchRequest request, IEnumerable<Journal> journalsInRepository)
-        {
-            CollectionAssert.IsNotEmpty(journalsInRepository, "Searching an empty repository is not a useful test");
-
-            using (var lifetime = AutofacConfiguration
-                .CreateDefaultContainerBuilder()
-                .WithNoLicensing()
-                .BuildSearchable(journalsInRepository))
-            {
-                return lifetime.Resolve<SearchController>().Search(request).Journals.ToList();
-            }
-        }
+        }        
     }
 }
