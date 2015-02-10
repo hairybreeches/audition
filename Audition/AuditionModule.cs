0b9bde64c76fc3a5612be742edde56595ed8b4a7
hairybreeches <hairybreeches@bitbucket.org>
2015-02-10 12:45:38 +0000

    make the webapp responsible for tying together all components that aren't chromium

--- Audition/AuditionModule.cs
+++ Audition/AuditionModule.cs
@@ -1,16 +1,6 @@
 ﻿using Audition.Chromium;
 using Autofac;
-using CsvExport;
-using ExcelImport;
-using Licensing;
-using Native;
-using Persistence;
-using Sage50;
-using SqlImport;
-using UserData;
 using Webapp;
-using Webapp.Session;
-using Module = Autofac.Module;
 
 namespace Audition
 {
@@ -18,17 +8,8 @@ namespace Audition
     {
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterModule<Sage50Module>();
-            builder.RegisterModule<NativeModule>();
+            builder.RegisterModule<WebappModule>();            
             builder.RegisterModule<ChromiumModule>();
-            builder.RegisterModule<CsvExportModule>();            
-            builder.RegisterModule<SessionModule>();            
-            builder.RegisterModule<PersistenceModule>();
-            builder.RegisterModule<HttpModule>();
-            builder.RegisterModule<LicensingModule>();
-            builder.RegisterModule<UserDataModule>();
-            builder.RegisterModule<ExcelImportModule>();
-            builder.RegisterModule<SqlImportModule>();
         }
     }
 }
