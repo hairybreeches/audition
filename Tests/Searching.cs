e7581d1063b8eca40580e02948506809c823e463
hairybreeches <hairybreeches@bitbucket.org>
2015-02-10 11:42:31 +0000

    update todo

--- Tests/Searching.cs
+++ Tests/Searching.cs
@@ -11,7 +11,11 @@ namespace Tests
     {
         public static IEnumerable<Journal> ExecuteSearch(ISearchWindow searchWindow, params Journal[] journalsInRepository)
         {
-            //todo: shouldn't have to use a filesystem here
+            //perhaps we could save a few seconds here by using a mock repository which stores the journals in memory.
+            //however, it's important that we have at least one test for each searcher 
+            //that makes sure the way we parse the journals is compatible with the way the repository delivers them 
+            //(eg no multiple enumeration of IEnumerables)
+            //and these tests are not currently slow enough to warrant sacrificing assurance
             var repo = new TempFileJournalRepository(new FileSystem());
             repo.UpdateJournals(journalsInRepository);
 
