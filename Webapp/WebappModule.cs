d2135833325992f673dc853f08ac0452490f7618
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-02-10 12:45:38 +0000

    make the webapp responsible for tying together all components that aren't chromium

---
+++ Webapp/WebappModule.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using CsvExport;
+using ExcelImport;
+using Licensing;
+using Native;
+using Persistence;
+using Sage50;
+using SqlImport;
+using UserData;
+using Webapp.Session;
+
+namespace Webapp
+{
+    public class WebappModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterModule<Sage50Module>();
+            builder.RegisterModule<NativeModule>();
+            builder.RegisterModule<CsvExportModule>();
+            builder.RegisterModule<SessionModule>();
+            builder.RegisterModule<PersistenceModule>();
+            builder.RegisterModule<HttpModule>();
+            builder.RegisterModule<LicensingModule>();
+            builder.RegisterModule<UserDataModule>();
+            builder.RegisterModule<ExcelImportModule>();
+            builder.RegisterModule<SqlImportModule>();
+        }
+    }
+}
