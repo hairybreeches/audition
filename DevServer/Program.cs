2cb68d938f2d271c9dcf0a7ae407f60b73db9086
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-02-12 11:54:50 +0000

    add dev server

--- DevServer/Program.cs
+++ DevServer/Program.cs
@@ -1,9 +1,37 @@
-﻿namespace DevServer
+﻿using System;
+using Autofac;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.Hosting;
+using Owin;
+using Webapp;
+
+namespace DevServer
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (var lifetime = ContainerBuilder())
+            using(WebApp.Start(Routing.InternalDomain, GetServerAction(lifetime)))
+            {
+                Console.WriteLine("Running...");
+                Console.ReadLine(); 
+            }
         }
+
+        private static IContainer ContainerBuilder()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<WebappModule>();
+            builder.Register(_ => new PhysicalFileSystem("../../../CompiledUI/UI")).As<IFileSystem>();
+            return builder.Build();
+        }
+
+        private static Action<IAppBuilder> GetServerAction(IComponentContext lifetime)
+        {
+            return lifetime.Resolve<OwinServerConfigurer>().Configure;
+        }
+
+
     }
 }
