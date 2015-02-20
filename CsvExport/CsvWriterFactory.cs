e71a3eaad6951199f40f2ff1187672ab011787ad
hairybreeches <hairybreeches@bitbucket.org>
2015-02-20 16:07:23 +0000

    extract responsibility for creating the csv writer

---
+++ CsvExport/CsvWriterFactory.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper;
+using Native.Disk;
+
+namespace CsvExport
+{
+    public class CsvWriterFactory
+    {
+        private readonly IFileSystem fileSystem;
+
+        public CsvWriterFactory(IFileSystem fileSystem)
+        {
+            this.fileSystem = fileSystem;
+        }
+
+        public ICsvWriter CreateWriter(string filename)
+        {
+            return new CsvWriter(fileSystem.OpenFileToWrite(filename));
+        }        
+    }
+}
