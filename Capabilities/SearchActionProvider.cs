f34656c39e3d9a0efb1cef29d0f94fa7f442e1c1
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-02-27 16:45:07 +0000

    add the capability

--- Capabilities/SearchActionProvider.cs
+++ Capabilities/SearchActionProvider.cs
@@ -11,6 +11,7 @@ namespace Capabilities
                 yield return new SearchAction("with round number endings", SearchActionName.Ending, MappingFields.Amount);
                 yield return new SearchAction("posted by unexpected users", SearchActionName.Users, MappingFields.Username);
                 yield return new SearchAction("posted to unusual nominal codes", SearchActionName.Accounts, MappingFields.NominalCode);
+                yield return new SearchAction("which are possible duplicates", SearchActionName.Duplicates, MappingFields.NominalCode, MappingFields.Amount);
             }
         }
     }
