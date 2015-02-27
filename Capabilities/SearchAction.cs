d57f996fabd05c231d9432ca1d8f32d472e309b3
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-02-27 16:35:08 +0000

    make it possible for a SearchAction to require more than one MappingField

--- Capabilities/SearchAction.cs
+++ Capabilities/SearchAction.cs
@@ -1,17 +1,21 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Capabilities
 {
     public class SearchAction
     {        
-        public SearchAction(string userFriendlyDescription, IMappingField requiredField, SearchActionName name)
+        public SearchAction(string userFriendlyDescription, SearchActionName name, params IMappingField[] requiredFields)
         {
-            ErrorMessage = String.Format("In order to search for transactions {0}, you must import transactions with a value for the {1}", userFriendlyDescription, requiredField);
-            RequiredField = requiredField;
+            ErrorMessage = String.Format("In order to search for transactions {0}, you must import transactions with a value for the {1}", 
+                userFriendlyDescription, 
+                String.Join(", the ", requiredFields.Select(x=>x.ToString())));
+            RequiredFields = requiredFields;
             Name = name;
         }
 
-        public IMappingField RequiredField { get; private set; }
+        public IEnumerable<IMappingField> RequiredFields { get; private set; }
         public string ErrorMessage { get; private set; }
         public SearchActionName Name { get; private set; }
     }
