a6d99cf2b2ce8d2ed94d56bf61d2836b544919d3
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-02-27 11:35:43 +0000

    test to show we can deserialise the import request

--- Tests/JsonConverterTests.cs
+++ Tests/JsonConverterTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Audition.Chromium;
 using Autofac;
+using ExcelImport;
 using Model.Time;
 using Newtonsoft.Json;
 using NodaTime;
@@ -53,6 +54,41 @@ namespace Tests
             Assert.AreEqual(new SearchRequest<UnusualAccountsParameters>(new SearchWindow<UnusualAccountsParameters>(new UnusualAccountsParameters(2),
                 new DateRange(new DateTime(2012, 4, 5), new DateTime(2013, 4, 4))), 7), 
                 result);
+        }
+
+        [Test]
+        public void CanDeserializeImportMapping()
+        {
+            var result = Parse<ExcelImportMapping>(@"{
+    'SheetDescription':{
+        'Filename':'C:\\code\\Audition\\build\\Debug\\Dev\\ExampleSage50Export.xlsx',
+        'UseHeaderRow':true,
+        'Sheet':2
+    },
+    'Lookups':{
+        'TransactionDate':2,
+        'Description':4,
+        'Username':8,
+        'AccountCode':16,
+        'AccountName':32,
+        'Amount':64,
+        'Id':128,
+        'Type':256}
+}");
+            var fieldLookups = new FieldLookups(transactionDate: 2,
+                description: 4,
+                username: 8,
+                accountCode: 16,
+                accountName: 32,
+                amount: 64,
+                id: 128,
+                type: 256);
+
+            Assert.AreEqual(fieldLookups,result.Lookups);
+            var sheetDescription = result.SheetDescription;
+            Assert.AreEqual(true,sheetDescription.UseHeaderRow);
+            Assert.AreEqual(2,result.SheetDescription.Sheet);
+            Assert.AreEqual(@"C:\code\Audition\build\Debug\Dev\ExampleSage50Export.xlsx",sheetDescription.Filename);
         }
 
        [Test]
