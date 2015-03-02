eb259405327a9e678ad55b43405e0fc3f93be37c
hairybreeches <hairybreeches@bitbucket.org>
2015-03-02 16:53:39 +0000

    these are both the same

--- ExcelImport/ExcelDataFileStorage.cs
+++ ExcelImport/ExcelDataFileStorage.cs
@@ -1,40 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Model;
-using UserData;
+﻿using UserData;
 
 namespace ExcelImport
 {
-    public class ExcelDataFileStorage
+    public class ExcelDataFileStorage : DataLocationStorage
     {
-
-        private readonly IUserDetailsStorage userDetailsStorage;
-        private readonly ExcelDemoDataSupplier demoDataSupplier;
-
         public ExcelDataFileStorage(IUserDetailsStorage userDetailsStorage, ExcelDemoDataSupplier demoDataSupplier)
+            :base(userDetailsStorage, demoDataSupplier, details => details.ExcelFiles, details => details.AddExcelFile)
         {
-            this.userDetailsStorage = userDetailsStorage;
-            this.demoDataSupplier = demoDataSupplier;
-        }
-
-        public UserDetails GetUserDetails()
-        {
-             return userDetailsStorage.Load();
-        }                
-
-        public void StoreUsage(ExcelImportMapping mapping)
-        {
-            var details = GetUserDetails();
-            details.AddExcelFile(mapping.SheetDescription.Filename);
-            userDetailsStorage.Save(details);
-        }
-
-        public IEnumerable<string> GetExcelDataFiles()
-        {
-            return GetUserDetails().ExcelFiles
-                .Concat(demoDataSupplier.GetDemoDataLocations())                
-                .Distinct(StringComparer.CurrentCultureIgnoreCase);
         }
     }
 }
