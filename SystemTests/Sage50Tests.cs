f84fbf0e8d150079683fb2ea02257caeb9def286
hairybreeches <hairybreeches@bitbucket.org>
2015-03-03 17:09:56 +0000

    make it optional to import archived data

--- SystemTests/Sage50Tests.cs
+++ SystemTests/Sage50Tests.cs
@@ -14,9 +14,35 @@ namespace SystemTests
         [Test]
         public void Sage50JournalSearchReturnsJournals()
         {
+            var result = GetJournalsImported(true, 128);
+
+            Assert.AreEqual("1278", result.TotalResults, "We should get all the transactions back");
+            Assert.AreEqual(
+                new Transaction("1236", new DateTime(2013, 3, 31), "MANAGER", "", "Journal Credit",
+                    new LedgerEntry("1230", "Petty Cash", LedgerEntryType.Cr, 100)
+                    ), result.Transactions[7], "A random transaction should be correct");
+        }      
+        
+        [Test]
+        public void ArchivedResultsNotReturnedWhenNotRequired()
+        {
+            var result = GetJournalsImported(false, 1);
+
+            Assert.AreEqual("1217", result.TotalResults, "We should get only get the unarchived transactions back");
+            Assert.AreEqual(
+                new Transaction("8", new DateTime(2013, 12, 31), "MANAGER", "Opening Balance", "Sales Credit Note",
+                    new LedgerEntry("1100", "Debtors Control Account", LedgerEntryType.Cr, 0.05m),
+                    new LedgerEntry("9998", "Suspense Account", LedgerEntryType.Dr, 0.05m),
+                    new LedgerEntry("2200", "Sales Tax Control Account", LedgerEntryType.Dr, 0)
+                    ), result.Transactions[7], "A random transaction should be correct");
+        }
+
+        private static SearchResponse GetJournalsImported(bool includeArchived, int pageNumber)
+        {
             //vagues search window ever, to return all transactions in the searcher
-            const string searchWindow = "{'Period':{'From':'1649-01-30', 'To':'4789-7-14'},'Parameters':{'users':'steve\r\nBarry'}";
-            const string searchRequest = "{pageNumber: 1, searchWindow: " + searchWindow + "}";
+            const string searchWindow =
+                "{'Period':{'From':'1649-01-30', 'To':'4789-7-14'},'Parameters':{'users':'steve\r\nBarry'}";
+            var searchRequest = "{pageNumber: " + pageNumber + ", searchWindow: " + searchWindow + "}";
 
             var requestResponse = new MockRequest("POST",
                 searchRequest,
@@ -27,22 +53,14 @@ namespace SystemTests
                 .Build())
             {
                 lifetime.ImportFromSage50(new Sage50ImportDetails
-                {                    
+                {
                     DataDirectory = @"Sage50SampleData",
-                    Username = "Manager"
+                    Username = "Manager",
+                    IncludeArchived = includeArchived
                 });
 
-                var result = lifetime.GetParsedResponseContent<SearchResponse>(requestResponse).Result;
-
-                Assert.AreEqual("1278", result.TotalResults, "We should get all the transactions back");
-                Assert.AreEqual(new Transaction("8", new DateTime(2013,12,31), "MANAGER", "Opening Balance", "Sales Credit Note",
-                    new LedgerEntry("1100", "Debtors Control Account", LedgerEntryType.Cr, 0.05m), 
-                    new LedgerEntry("9998", "Suspense Account", LedgerEntryType.Dr, 0.05m), 
-                    new LedgerEntry("2200", "Sales Tax Control Account", LedgerEntryType.Dr, 0)
-                ), result.Transactions[7], "A random transaction should be correct");
+                return lifetime.GetParsedResponseContent<SearchResponse>(requestResponse).Result;                
             }
-
-
-        }       
+        }
     }
 }
