c820d3dbe89f9e24133ebba9cafa412ad2838a85
hairybreeches <hairybreeches@bitbucket.org>
2015-03-03 10:54:13 +0000

    rename fields

--- Capabilities/DisplayFieldProvider.cs
+++ Capabilities/DisplayFieldProvider.cs
@@ -8,9 +8,9 @@ namespace Capabilities
         public DisplayField Username = new DisplayField(DisplayFieldName.Username, MappingFields.Username, entry => entry.Username);
         public DisplayField Description = new DisplayField(DisplayFieldName.Description, MappingFields.Description, entry => entry.Description);
         public DisplayField LedgerEntryType = new DisplayField(DisplayFieldName.LedgerEntryType, MappingFields.Amount, entry => entry.LedgerEntryType);
-        public DisplayField AccountName = new DisplayField(DisplayFieldName.NominalName, MappingFields.NominalName, entry => entry.NominalCodeName);
+        public DisplayField NominalName = new DisplayField(DisplayFieldName.NominalName, MappingFields.NominalName, entry => entry.NominalCodeName);
         public DisplayField Amount = new DisplayField(DisplayFieldName.Amount, MappingFields.Amount, entry => entry.Amount);
-        public DisplayField AccountCode = new DisplayField(DisplayFieldName.NominalCode, MappingFields.NominalCode, entry => entry.NominalCode);
+        public DisplayField NominalCode = new DisplayField(DisplayFieldName.NominalCode, MappingFields.NominalCode, entry => entry.NominalCode);
         public DisplayField Id = new DisplayField(DisplayFieldName.Id, MappingFields.Id, entry => entry.TransactionId);
         public DisplayField TransactionType = new DisplayField(DisplayFieldName.Type, MappingFields.Type, entry => entry.TransactionType);
 
@@ -22,9 +22,9 @@ namespace Capabilities
                 yield return Description;
                 yield return Username;
                 yield return LedgerEntryType;
-                yield return AccountName;
+                yield return NominalName;
                 yield return Amount;
-                yield return AccountCode;
+                yield return NominalCode;
                 yield return Id;
                 yield return TransactionType;
             }
