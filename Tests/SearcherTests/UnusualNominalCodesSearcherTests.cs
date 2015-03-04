9a03510cdaa96088ac4e912c49e213ef67a214ff
hairybreeches <hairybreeches@bitbucket.org>
2015-03-04 16:32:58 +0000

    order the transactions for the unusual nominal code by which code it is

--- Tests/SearcherTests/UnusualNominalCodesSearcherTests.cs
+++ Tests/SearcherTests/UnusualNominalCodesSearcherTests.cs
@@ -30,20 +30,23 @@ namespace Tests.SearcherTests
         }        
 
         [Test]
-        public void DoesNotReturnDuplicatesWhenTransactionsPostedToTwoUnusualNominalCodes()
+        public void ReturnsDuplicatesWhenTransactionsPostedToTwoUnusualNominalCodes()
         {
             //given one transaction which includes a line to two rare nominal codes (just one posting each)
-            var transactionPostedToUncommonNominalCode = PostedTo("d", "e");
+            var postedToTwoUnusualNominalCodes = PostedTo("d", "e");
 
-            //and a search window for the period "all time", for transactions to nominal codes with <2 postings
-            var searchWindow = new SearchWindow<UnusualNominalCodesParameters>(new UnusualNominalCodesParameters(2),
+            var postedToUnusualCode1 = PostedTo("a", "d");
+            var postedToUnusualCode2 = PostedTo("b", "e");
+
+            //and a search window for the period "all time", for transactions to nominal codes with <3 postings
+            var searchWindow = new SearchWindow<UnusualNominalCodesParameters>(new UnusualNominalCodesParameters(3),
                 new DateRange(new DateTime(1, 1, 1), new DateTime(3000, 12, 31)));
 
             //when we do the transaction search
-            var transactions = Searching.ExecuteSearch(searchWindow, PostedTo("a", "b"), PostedTo("b", "a"), transactionPostedToUncommonNominalCode).ToList();
+            var transactions = Searching.ExecuteSearch(searchWindow, postedToUnusualCode1, postedToUnusualCode2, PostedTo("a", "b"), PostedTo("b", "a"), postedToTwoUnusualNominalCodes).ToList();
 
-            //we end up with only one copy of the expected transaction - it's not repeated for each rare nominal code it's been posted to.
-            CollectionAssert.AreEquivalent(new []{transactionPostedToUncommonNominalCode}, transactions);
+            //the transaction posted to two unusual codes appears twice, once in the list for nominal code 1 and once in the list for nominal code 2
+            CollectionAssert.AreEqual(new []{postedToUnusualCode1, postedToTwoUnusualNominalCodes, postedToUnusualCode2, postedToTwoUnusualNominalCodes}, transactions);
         }
         
         [Test]
@@ -78,6 +81,8 @@ namespace Tests.SearcherTests
             var transactions = Searching.ExecuteSearch(searchWindow, transaction,
                 PostedTo("a", "b", new DateTime(2000, 1, 1)),
                 PostedTo("a", "b", new DateTime(1998, 12, 31)),
+                PostedTo("a", "c", new DateTime(1999, 12, 31)),
+                PostedTo("a", "c", new DateTime(1999, 12, 31)),
                 PostedTo("a", "b", new DateTime(2000, 1, 1))).ToList();
 
             //those transactions posted to the nominal code in other periods don't make the code any less unusual.
