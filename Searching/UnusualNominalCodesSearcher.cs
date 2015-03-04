7ae94f211792a644a42daa0baa3704785fa86fef
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-03-04 16:32:58 +0000

    order the transactions for the unusual nominal code by which code it is

--- Searching/UnusualNominalCodesSearcher.cs
+++ Searching/UnusualNominalCodesSearcher.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using Model.Accounting;
-using Model.Time;
-using Persistence;
 using Searching.SearchWindows;
 
 namespace Searching
@@ -14,7 +12,33 @@ namespace Searching
             var lookup = new NominalCodeLookup(transactions);
             var unusualNominalCodes = lookup.UnusualNominalCodes(parameters.MinimumEntriesToBeConsideredNormal);
             return transactions
-                .Where(transaction=>transaction.Lines.Any(line => unusualNominalCodes.Contains(line.NominalCode)));
+                .SelectMany(transaction => GetTransactionAndReason(transaction, unusualNominalCodes))
+                .OrderBy(x=>x.Reason)
+                .Select(x=>x.Transaction);
+        }
+
+        private IEnumerable<TransactionAndReason> GetTransactionAndReason(Transaction transaction, ISet<string> unusualNominalCodes)
+        {            
+            foreach (var line in transaction.Lines)
+            {
+                var nominalCode = line.NominalCode;
+                if (unusualNominalCodes.Contains(nominalCode))
+                {
+                    yield return new TransactionAndReason(transaction, nominalCode);
+                }
+            }
+        }
+
+        private class TransactionAndReason
+        {
+            public TransactionAndReason(Transaction transaction, string reason)
+            {
+                Transaction = transaction;
+                Reason = reason;
+            }
+
+            public Transaction Transaction {get; private set; }
+            public string Reason {get; private set; }
         }
     }
 }
