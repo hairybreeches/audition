f4b96d4dab62aff596fd9b84361558950e493f75
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-03-13 16:59:01 +0000

    get rid of this stupid return value

--- ExcelFormatting/DateColumnFormatter.cs
+++ ExcelFormatting/DateColumnFormatter.cs
@@ -4,11 +4,10 @@ namespace ExcelFormatting
 {
     public class DateColumnFormatter : IExcelColumnFormatter
     {
-        public int FormatColumn(Worksheet worksheet, int columnIndex)
+        public void FormatColumn(Worksheet worksheet, int columnIndex)
         {
             var range = GetColumnDataCells(worksheet, columnIndex);
             range.NumberFormat = "DD/MM/YYYY";
-            return columnIndex + 1; 
         }
 
         private Range GetColumnDataCells(Worksheet sheet, int columnIndex)
--- ExcelFormatting/NoFormattingRequiredFormatter.cs
+++ ExcelFormatting/NoFormattingRequiredFormatter.cs
@@ -4,9 +4,8 @@ namespace ExcelFormatting
 {
     public class NoFormattingRequiredFormatter : IExcelColumnFormatter
     {
-        public int FormatColumn(Worksheet worksheet, int columnIndex)
+        public void FormatColumn(Worksheet worksheet, int columnIndex)
         {
-            return columnIndex + 1;
         }
     }
 }
