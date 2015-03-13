b534df65370689d403abbc100720d504eff93a9b
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-03-13 16:57:10 +0000

    make the display fields themselves provide the formatters too

--- ExcelExport/ExcelExportModule.cs
+++ ExcelExport/ExcelExportModule.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using Autofac;
-using Capabilities;
+﻿using Autofac;
 using CsvExport;
-using ExcelFormatting;
-using Model;
 
 namespace ExcelExport
 {
@@ -14,25 +9,6 @@ namespace ExcelExport
         {
             builder.RegisterType<ExcelExporter>().As<ITransactionExporter>(); 
             builder.RegisterType<ExcelFileOpener>();
-            builder.Register(CreateColumns).As<IEnumerable<IFormatterFactory>>();
-        }
-
-        private static ColumnFactory[] CreateColumns(IComponentContext context)
-        {
-            var fieldProvider = context.Resolve<DisplayFieldProvider>();
-
-            return new[]
-            {
-                new ColumnFactory(fieldProvider.Id),
-                new ColumnFactory(fieldProvider.TransactionDate, new DateColumnFormatter()),
-                new ColumnFactory(fieldProvider.TransactionType),
-                new ColumnFactory(fieldProvider.Description),
-                new ColumnFactory(fieldProvider.Username),                               
-                new ColumnFactory(fieldProvider.NominalCode),
-                new ColumnFactory(fieldProvider.NominalName),
-                new ColumnFactory(fieldProvider.LedgerEntryType),
-                new ColumnFactory(fieldProvider.Amount)
-            };
         }
     }
 }
